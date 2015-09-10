80e9c73db7f2fc7db22b2c936dc6c9e2e8700dc5
SMOKE <metallica5875@yahoo.com>
2015-09-10 17:41:41 -0400

    Added missing license

--- include/psp2/common_api.h
+++ include/psp2/common_api.h
@@ -1,3 +1,14 @@
+/**
+ * \file
+ * \brief Header file which defines common dialog functions used along with message dialogs
+ *
+ * Copyright (C) 2015 PSP2SDK Project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef _COMMON_API_H_
 #define _COMMON_API_H_
 
--- include/psp2/message_dialog.h
+++ include/psp2/message_dialog.h
@@ -1,3 +1,14 @@
+/**
+ * \file
+ * \brief Header file which defines message dialog functions
+ *
+ * Copyright (C) 2015 PSP2SDK Project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #ifndef _MSG_DIALOG_H_
 #define _MSG_DIALOG_H_
 
