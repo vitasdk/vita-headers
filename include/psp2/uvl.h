97cd8e7d96e01f1435497854c8ca0363a9c1202b
TheOfficialFloW <theofficialflow1996@gmail.com>
2016-01-26 16:59:42 +0100

    Added missing UVL export

--- include/psp2/uvl.h
+++ include/psp2/uvl.h
@@ -6,12 +6,13 @@ extern "C" {
 #endif
 
 void *uvl_alloc_code_mem(unsigned int *p_len);
-void uvl_unlock_mem(void);
-void uvl_lock_mem(void);
+int uvl_debug_log(const char *line);
+void uvl_exit(int status);
 void uvl_flush_icache(void *addr, unsigned int len);
 int uvl_load(const char *path);
-void uvl_exit(int status);
+void uvl_lock_mem(void);
 int uvl_log_write(const void* buffer, unsigned int size);
+void uvl_unlock_mem(void);
 
 #ifdef __cplusplus
 }
