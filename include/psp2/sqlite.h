79e8bd8db585e5a6eca3265892ce9f24dacc0d55
Rinnegatamante <rinnegatamante@gmail.com>
2016-08-09 00:11:09 +0200

    Added SceSqlite_stub references. (#17)
    * Added SceSqlite_stub references.
    
    Added SceSqlite_stub references.
    
    * Brief fix.
    
    Brief fix.

---
+++ include/psp2/sqlite.h
@@ -0,0 +1,38 @@
+/**
+ * \file
+ * \brief Header file which defines sqlite related variables and functions
+ *
+ * Copyright (C) 2015 PSP2SDK Project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef _PSP2_SQLITE_H_
+#define _PSP2_SQLITE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct{
+	void *(*xMalloc)(int);
+	void *(*xRealloc)(void*,int);
+	void (*xFree)(void*);
+}SceSqliteMallocMethods;
+
+/**
+ * Wrapper for sqlite3_config(SQLITE_CONFIG_MALLOC)
+ *
+ * @param[in] methods - A proper set of memory allocation methods
+ *
+ * @return 0 on success, < 0 on error.
+*/
+int sceSqliteConfigMallocMethods(SceSqliteMallocMethods* methods);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP2_SQLITE_H_ */
