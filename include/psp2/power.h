40503c74ad562a764d96519ace77a35b3d999c02
Rinnegatamante <rinnegatamante@gmail.com>
2016-09-04 13:38:45 +0200

    Added scePowerSetBusClockFrequency and scePowerSetGpuClockFrequency.

--- include/psp2/power.h
+++ include/psp2/power.h
@@ -36,7 +36,9 @@ int scePowerGetBatteryFullCapacity(); //?
 int scePowerGetArmClockFrequency(); //Get Clock Frequency of the ARM
 int scePowerGetBusClockFrequency(); //Get Clock Frequency of the BUS
 int scePowerGetGpuClockFrequency(); //Get Clock Frequency of the Gpu
-int scePowerSetArmClockFrequency(int freq); // Set Cet Clock Frequency of the ARM
+int scePowerSetArmClockFrequency(int freq); // Set Clock Frequency of the ARM
+int scePowerSetBusClockFrequency(int freq); // Set Clock Frequency of the BUS
+int scePowerSetGpuClockFrequency(int freq); // Set Clock Frequency of the Gpu
 
 #ifdef __cplusplus
 }
