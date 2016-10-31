9f159be4fad7327296b76e8ec2e9f37f042efb7f
Yifan Lu <me@yifanlu.com>
2016-10-30 21:42:03 -0700

    More kernel adds

---
+++ include/psp2kern/kernel/processmgr.h
@@ -0,0 +1,27 @@
+/**
+ * \file
+ * \brief Header file which defines processes related variables and functions for kernel
+ *
+ * Copyright (C) 2015 PSP2SDK Project
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef _PSP2_KERNEL_PROCESSMGR_H_
+#define _PSP2_KERNEL_PROCESSMGR_H_
+
+#include <psp2/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void *sceKernelGetProcessKernelBuf(SceUID pid);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP2_KERNEL_PROCESSMGR_H_ */
