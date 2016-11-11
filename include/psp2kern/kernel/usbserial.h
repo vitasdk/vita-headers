1cea51b37d0d730ddb635e6370753bc7ecc082b4
geekboy1011 <geekboy1011@gmail.com>
2016-11-10 21:28:42 -0500

    Split usbserial.h to psp2kern/kernel/usbserial.h fix some documentation.

--- include/psp2kern/kernel/usbserial.h
+++ include/psp2kern/kernel/usbserial.h
@@ -23,57 +23,47 @@ extern "C" {
  * @return 0 on success, < 0 on error
  *
  * @note The user is responsible for making sure no other UDC driver is active. This activates UDC as a "PS Vita" Type D device. 
- *
- * @note Wrapper for kernel function sceUsbSerialStartForDriver()
  */
-int sceUsbSerialStart(void);
+int sceUsbSerialStartForDriver(void);
 
 /**
  * Setup USB serial device
  *
  * @return 0 on success, < 0 on error
  *
- * @param unk Unknown 
- *
- * @note Wrapper for kernel function sceUsbSerialSetupForDriver()
+ * @param unk Unknown
  */
-int sceUsbSerialSetup(int unk);	
+int sceUsbSerialSetupForDriver(int unk);
 
 /**
- * Close USB serial device
- *
- * @note Wrapper for kernel function sceUsbSerialCloseForDriver()
- */
-int sceUsbSerialClose(void);
-
-/**
- * Close USB serial device
+ * Get USB device status
  *
  * @return Returns 1 when serial port is open
- *
- * @note Wrapper for kernel function sceUsbSerialStatusForDriver()
  */
-int sceUsbSerialStatus(void);
+int sceUsbSerialStatusForDriver(void);
+
+/**
+ * Close USB serial device
+ */
+int sceUsbSerialCloseForDriver(void);
 
 /**
  * Get receive buffer length
  *
  * @return Returns length of USB rx buffer
- *
- * @note Wrapper for kernel function sceUsbSerialGetLenForDriver()
  */
-unsigned int sceUsbSerialGetLen(void);
+unsigned int sceUsbSerialGetLenForDriver(void);
 
 /**
  * Send data
+ *
  * @param[in] *buffer data to send over usb
  * @param len int len of serial buffer data 
  * @param unk1 Unknown
  * @param unk2 Unknown
  * @note max send length is 0x10000
- * @note Wrapper for kernel function sceUsbSerialSendForDriver()
  */
-unsigned int sceUsbSerialSend(const void *buffer, unsigned int len, int unk1, int unk2);
+unsigned int sceUsbSerialSendForDriver(const void *buffer, unsigned int len, int unk1, int unk2);	
 
 /**
  * Receive Data
@@ -83,9 +73,8 @@ unsigned int sceUsbSerialSend(const void *buffer, unsigned int len, int unk1, in
  * @param unk1 Unknown
  * @param unk2 Unknown
  * @note max recv length is 0x10000
- * @note Wrapper for kernel function sceUsbSerialRecvForDriver()
  */
-unsigned int sceUsbSerialRecv(void *buffer, unsigned int max_len, int unk1, int unk2);
+unsigned int sceUsbSerialRecvForDriver(void *buffer, unsigned int max_len, int unk1, int unk2);
 
 #ifdef __cplusplus
 }
