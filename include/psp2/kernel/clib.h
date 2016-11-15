cab973736952531f06a57687901f36beab6f33f3
cpasjuste <cpasjuste@gmail.com>
2016-11-15 09:57:00 +0100

    psp2/kernel/clib.h: add a few defines

--- include/psp2/kernel/clib.h
+++ include/psp2/kernel/clib.h
@@ -20,9 +20,20 @@ extern "C" {
 #endif
 
 int sceClibStrcmp(const char *, const char *);
+void *sceClibStrncmp(const char *, const char *, SceSize);
+int sceClibStrncasecmp(const char *, const char *, SceSize);
+char *sceClibStrncpy(char *, const char *, SceSize);
+char *sceClibStrncat(char *, const char *, SceSize);
+SceSize sceClibStrnlen(const char *, SceSize);
+char *sceClibStrrchr(const char *, int);
 
+int sceClibPrintf(const char *, ...);
 int sceClibSnprintf(char *, SceSize, const char *, ...);
 int sceClibVsnprintf(char *, SceSize, const char *, va_list);
+
+void *sceClibMemset(void *, int, SceSize);
+void *sceClibMemcpy(void *, const void *, SceSize);
+void *sceClibMemmove(void *, const void *, SceSize);
 
 #ifdef __cplusplus
 }
