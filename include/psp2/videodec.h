dff4d388fd1a14f43cf4e6eee17dec7403121300
Sunguk Lee <d3m3vilurr@gmail.com>
2016-12-16 01:50:45 +0900

    Add videodec.h (#112)
    * Copy videodec.h from RPCS3
    
    https://github.com/RPCS3/rpcs3/blob/9c30de6fe/rpcs3/Emu/PSP2/Modules/sceVideodec.h
    
    * Change variable types and add function difinitions
    
    * Define decoding codec type
    
    * Update function difinitions & documents
    
    * Fix 0x8062000E at video decoding
    
    * Define SceVideodec document group
    
    * Change fieldname in SceAvcdecInfo
    
    change `padding` to `unknown` and add comment

---
+++ include/psp2/videodec.h
@@ -0,0 +1,193 @@
+/**
+ * \usergroup{SceVideoDec}
+ * \usage{psp2/videodec.h,-lSceVideodec_stub}
+ */
+
+/**
+ * \file
+ * \brief Header file which defines video decoder related variables and functions
+ *
+ * Copyright (C) 2016 Ivan
+ * Copyright (C) 2016 vitasdk
+ *
+ * This header file is licensed under the MIT license.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef _PSP2_VIDEODEC_H_
+#define _PSP2_VIDEODEC_H_
+
+#include <psp2/types.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+	SCE_VIDEODEC_TYPE_HW_AVCDEC = 0x1001,
+} SceVideodecType;
+
+typedef struct SceVideodecQueryInitInfoHwAvcdec {
+	uint32_t size;				//!< struct size
+	uint32_t horizontal;
+	uint32_t vertical;
+	uint32_t numOfRefFrames;
+	uint32_t numOfStreams;
+} SceVideodecQueryInitInfoHwAvcdec;
+
+typedef union SceVideodecQueryInitInfo {
+	uint8_t reserved[32];
+	SceVideodecQueryInitInfoHwAvcdec hwAvc;
+} SceVideodecQueryInitInfo;
+
+typedef struct SceVideodecTimeStamp {
+	uint32_t upper;
+	uint32_t lower;
+} SceVideodecTimeStamp;
+
+typedef struct SceAvcdecQueryDecoderInfo {
+	uint32_t horizontal;
+	uint32_t vertical;
+	uint32_t numOfRefFrames;
+} SceAvcdecQueryDecoderInfo;
+
+typedef struct SceAvcdecDecoderInfo {
+	uint32_t frameMemSize;
+} SceAvcdecDecoderInfo;
+
+typedef struct SceAvcdecBuf {
+	void *pBuf;
+	uint32_t size;
+} SceAvcdecBuf;
+
+typedef struct SceAvcdecCtrl {
+	uint32_t handle;
+	SceAvcdecBuf frameBuf;
+} SceAvcdecCtrl;
+
+typedef struct SceAvcdecAu {
+	SceVideodecTimeStamp pts;
+	SceVideodecTimeStamp dts;
+	SceAvcdecBuf es;
+} SceAvcdecAu;
+
+typedef struct SceAvcdecInfo {
+	uint32_t numUnitsInTick;
+	uint32_t timeScale;
+	uint8_t fixedFrameRateFlag;
+
+	uint8_t aspectRatioIdc;
+	uint16_t sarWidth;
+	uint16_t sarHeight;
+
+	uint8_t colourPrimaries;
+	uint8_t transferCharacteristics;
+	uint8_t matrixCoefficients;
+
+	uint8_t videoFullRangeFlag;
+
+	uint8_t unknown[3]; // maybe picStruct & ctType
+
+	SceVideodecTimeStamp pts;
+} SceAvcdecInfo;
+
+typedef struct SceAvcdecFrameOptionRGBA {
+	uint8_t alpha;
+	uint8_t cscCoefficient;
+	uint8_t reserved[14];
+} SceAvcdecFrameOptionRGBA;
+
+typedef union SceAvcdecFrameOption {
+	uint8_t reserved[16];
+	SceAvcdecFrameOptionRGBA rgba;
+} SceAvcdecFrameOption;
+
+typedef struct SceAvcdecFrame {
+	uint32_t pixelType;		//!< display pixel format
+	uint32_t framePitch;
+	uint32_t frameWidth;
+	uint32_t frameHeight;
+
+	uint32_t horizontalSize;
+	uint32_t verticalSize;
+
+	uint32_t frameCropLeftOffset;
+	uint32_t frameCropRightOffset;
+	uint32_t frameCropTopOffset;
+	uint32_t frameCropBottomOffset;
+
+	SceAvcdecFrameOption opt;
+
+	void *pPicture[2];		//!< address of picture buffer
+} SceAvcdecFrame;
+
+typedef struct SceAvcdecPicture {
+	uint32_t size;
+	SceAvcdecFrame frame;
+	SceAvcdecInfo info;
+} SceAvcdecPicture;
+
+typedef struct SceAvcdecArrayPicture {
+	uint32_t numOfOutput;
+	uint32_t numOfElm;
+	SceAvcdecPicture **pPicture;
+} SceAvcdecArrayPicture;
+
+/***
+ * @param[in] codec - See ::SceVideodecType
+ * @param[in] initInfo - See ::SceVideodecQueryInitInfoHwAvcdec
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int sceVideodecInitLibrary(SceVideodecType codec, const SceVideodecQueryInitInfoHwAvcdec *initInfo);
+
+/***
+ * @param[in] codec - See ::SceVideodecType
+ * @param[in] query - Should be copy from ::SceVideodecQueryInitInfoHwAvcdec.
+ * @param[out] decoderInfo - Must be initialized with zeros.
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int sceAvcdecQueryDecoderMemSize(SceVideodecType codec, const SceAvcdecQueryDecoderInfo *query, SceAvcdecDecoderInfo *decoderInfo);
+
+/***
+ * @param[in] codec - See ::SceVideodecType
+ * @param[out] decoder - Must be initialized frameBuf.size and frameBuf.pBuf
+ * @param[in] query - Should be copy from ::SceVideodecQueryInitInfoHwAvcdec.
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int sceAvcdecCreateDecoder(SceVideodecType codec, SceAvcdecCtrl *decoder, const SceAvcdecQueryDecoderInfo *query);
+
+/***
+ * @param[in] decoder
+ * @param[in] au - Must set buffer information(es) and be initialized timestamps with 0xFFFFFFFF
+ * @param[out] array_picture - Must set numOfElm and pPicture. See ::SceAvcdecArrayPicture
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int sceAvcdecDecode(const SceAvcdecCtrl *decoder, const SceAvcdecAu *au, SceAvcdecArrayPicture *array_picture);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP2_VIDEODEC_H_ */
