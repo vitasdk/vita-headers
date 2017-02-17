fb1201dbcf20b3cf121a5337226ee4f52be30d82
Travis CI <builds@travis-ci.com>
2017-02-17 22:15:28 +0000

    Generated documents from TravisCI for build 163

--- group__SceUsbdUser_structSceUsbdDeviceInfo.js
+++ group__SceUsbdUser_structSceUsbdDeviceInfo.js
@@ -2,6 +2,5 @@ var group__SceUsbdUser_structSceUsbdDeviceInfo =
 [
     [ "device_id", "group__SceUsbdUser.html#gaae702a449230b997ec87fd71ace8b4f8", null ],
     [ "unk0", "group__SceUsbdUser.html#gafba9f42a7bbed208fb02713aead63477", null ],
-    [ "unk1", "group__SceUsbdUser.html#gacc00a1a1a093915f9b29960a0abfbc82", null ],
     [ "unk2", "group__SceUsbdUser.html#gab3548651e4dfc07a10f808c78e77e270", null ]
 ];
