202acb8176136fe8f23f70c98d310c1d580a0690
devnoname120 <devnoname120@gmail.com>
2017-04-10 01:13:51 +0200

    Add some brightness functions

---
+++ include/psp2/avconfig.h
@@ -0,0 +1,40 @@
+﻿/**
+ * \usergroup{SceAVConfig}
+ * \usage{psp2/avconfig.h,-lSceAVConfig_stub}
+ */
+
+
+#ifndef _PSP2_AVCONFIG_H_
+#define _PSP2_AVCONFIG_H_
+
+#include <psp2/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/***
+ * Get the maximum brightness.
+ *
+ * @param[out] maxBrightness - Maximum brightness.
+ * 
+ * @return 0 on success, < 0 on error.
+ */
+int sceAVConfigGetDisplayMaxBrightness(int *maxBrightness);
+
+/***
+ * Set the screen brightness.
+ *
+ * @param brightness - Brightness that the screen will be set to (range 21-65536, 0 turns off the screen).
+ * 
+ * @return 0 on success, < 0 on error.
+ */
+int sceAVConfigSetDisplayBrightness(int brightness);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
