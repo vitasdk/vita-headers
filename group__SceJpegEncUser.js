cfbb94e1acc3dc7186a95f399ba9261db9941d45
Travis CI <builds@travis-ci.com>
2017-04-10 21:47:46 +0000

    Generated documents from TravisCI for build 250

--- group__SceJpegEncUser.js
+++ group__SceJpegEncUser.js
@@ -1,10 +1,10 @@
 var group__SceJpegEncUser =
 [
-    [ "sceJpegEncoderCsc", "group__SceJpegEncUser.html#gae6f4c24704615070f1a4feac8bda49ae", null ],
+    [ "sceJpegEncoderCsc", "group__SceJpegEncUser.html#ga7d32b94253c6b509c781abcc0f490e8e", null ],
     [ "sceJpegEncoderEncode", "group__SceJpegEncUser.html#ga51d97e72ae0ff6996267c983627a0854", null ],
     [ "sceJpegEncoderEnd", "group__SceJpegEncUser.html#ga02de151671088a0fc05d395a36ac6fbe", null ],
     [ "sceJpegEncoderGetContextSize", "group__SceJpegEncUser.html#gac8866ee27a4f90c9c06287d451d6c17c", null ],
-    [ "sceJpegEncoderInit", "group__SceJpegEncUser.html#ga63e349420730a8520712029cdacd1aba", null ],
+    [ "sceJpegEncoderInit", "group__SceJpegEncUser.html#ga021a5c0080a6815131f917d6bbefac8d", null ],
     [ "sceJpegEncoderSetCompressionRatio", "group__SceJpegEncUser.html#ga6b039832b2340bcc8e2a49f283090799", null ],
     [ "sceJpegEncoderSetOutputAddr", "group__SceJpegEncUser.html#ga45d153069910d9b91c5f02f9103805e6", null ],
     [ "sceJpegEncoderSetValidRegion", "group__SceJpegEncUser.html#ga30272414b239976bb406d75f9b7fe9ce", null ]
