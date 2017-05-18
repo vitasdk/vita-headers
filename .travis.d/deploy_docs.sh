d18708ba8726b0807f5d76f218814108a0adafe1
Sunguk Lee <d3m3vilurr@gmail.com>
2017-05-19 07:03:03 +0900

    Add custom doxygen footer (#174)
    * Add custom footer
    * Remove by doxygen info

--- .travis.d/deploy_docs.sh
+++ .travis.d/deploy_docs.sh
@@ -13,5 +13,8 @@ mv docs/CNAME .
 mv docs/html/* .
 rm -rf docs
 git add *
+if [[ $(git status -s | grep -v "docs/CNAME" | wc -l) -eq "0" ]]; then
+    exit
+fi
 git commit -am "Generated documents from TravisCI for build $TRAVIS_BUILD_NUMBER"
 git push -q https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git gh-pages:gh-pages -f 2> /dev/null
