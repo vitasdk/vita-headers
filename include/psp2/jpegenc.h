29906411142b472f2e79673773d91e47bea39dd2
Rinnegatamante <rinnegatamante@gmail.com>
2017-06-17 11:46:00 +0200

    Renamed enums values, typo fix in jpegenc.h.

--- include/psp2/jpegenc.h
+++ include/psp2/jpegenc.h
@@ -13,7 +13,7 @@
 typedef enum SceJpegEncErrorCode {
 	SCE_JPEGENC_ERROR_IMAGE_SIZE                = 0x80650200,
 	SCE_JPEGENC_ERROR_INSUFFICIENT_BUFFER       = 0x80650201,
-	SCE_JPEGNEC_ERROR_INVALID_COMPRATIO         = 0x80650202,
+	SCE_JPEGENC_ERROR_INVALID_COMPRATIO         = 0x80650202,
 	SCE_JPEGENC_ERROR_INVALID_PIXELFORMAT       = 0x80650203,
 	SCE_JPEGENC_ERROR_INVALID_HEADER_MODE       = 0x80650204,
 	SCE_JPEGENC_ERROR_INVALID_POINTER           = 0x80650205,
@@ -21,15 +21,15 @@ typedef enum SceJpegEncErrorCode {
 } SceJpegEncErrorCode;
 
 typedef enum SceJpegEncoderPixelFormat {
-	PIXELFORMAT_ARGB8888 = 0,       //!< ARGB8888 format
-	PIXELFORMAT_YCBCR420 = 8,       //!< YCbCr420 format
-	PIXELFORMAT_YCBCR422 = 9,       //!< YCbCr422 format
-	PIXELFORMAT_CSC_ARGB_YCBCR = 16 //!< ARGB to YCbCr color conversion flag
+	SCE_JPEGENC_PIXELFORMAT_ARGB8888 = 0,       //!< ARGB8888 format
+	SCE_JPEGENC_PIXELFORMAT_YCBCR420 = 8,       //!< YCbCr420 format
+	SCE_JPEGENC_PIXELFORMAT_YCBCR422 = 9,       //!< YCbCr422 format
+	SCE_JPEGENC_PIXELFORMAT_CSC_ARGB_YCBCR = 16 //!< ARGB to YCbCr color conversion flag
 } SceJpegEncoderPixelFormat;
 
 typedef enum SceJpegEncoderHeaderMode {
-	HEADER_MODE_JPEG = 0,   //!< JPEG header mode
-	HEADER_MODE_MJPEG = 1   //!< MJPEG header mode
+	SCE_JPEGENC_HEADER_MODE_JPEG = 0,   //!< JPEG header mode
+	SCE_JPEGENC_HEADER_MODE_MJPEG = 1   //!< MJPEG header mode
 } SceJpegEncoderHeaderMode;
 
 /**
