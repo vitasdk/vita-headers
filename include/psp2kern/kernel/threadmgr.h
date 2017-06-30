d0aee314a26b323300bf8a6f250637b234682d58
codestation <codestation404@gmail.com>
2017-06-30 15:58:11 -0500

    The sceKernelGetMutexInfo second arg should be a pointer to SceKernelMutexInfo

--- include/psp2kern/kernel/threadmgr.h
+++ include/psp2kern/kernel/threadmgr.h
@@ -575,7 +575,7 @@ int ksceKernelCancelMutex(SceUID mutexid, int newCount, int *numWaitThreads);
  *
  * @return < 0 on error.
  */
-int ksceKernelGetMutexInfo(SceUID mutexid, SceKernelMutexInfo info);
+int ksceKernelGetMutexInfo(SceUID mutexid, SceKernelMutexInfo *info);
 
 
 /* Event flags. */
