b15d320e8c3340d31856a812e9c34554603b0a8e
Sergi Granell <xerpi.g.12@gmail.com>
2017-07-11 01:26:22 +0200

    Update I2C

--- include/psp2kern/lowio/i2c.h
+++ include/psp2kern/lowio/i2c.h
@@ -15,24 +15,26 @@ extern "C" {
 typedef enum SceI2cErrorCode {
 	SCE_I2C_ERROR_INVALID_BUS	= 0x803F0300,
 	SCE_I2C_ERROR_INVALID_SIZE	= 0x803F0302,
-	SCE_I2C_ERROR_INVALID_DEVICE	= 0x803F0303
+	SCE_I2C_ERROR_INVALID_ADDR	= 0x803F0303
 } SceI2cErrorCode;
 
 typedef struct SceI2cDebugHandlers {
 	unsigned int size;
-	void (*write_start)(int bus, int device, unsigned char *buffer, int size);
-	void (*write_error)(int bus, int error, int result);
-	void (*read_start)(int bus, int device, unsigned char *buffer, int size);
-	void (*read_error)(int bus, int error, int result);
-	void (*write_read_start)(int bus, int write_device, unsigned char *write_buffer, int write_size);
-	void (*write_read_error)(int bus, int error, int result);
+	void (*write_start)(int bus, int addr, unsigned char *buffer, int size);
+	void (*write_end)(int bus, int error, int result);
+	void (*read_start)(int bus, int addr, unsigned char *buffer, int size);
+	void (*read_end)(int bus, int error, int result);
+	void (*write_read_start)(int bus, int write_addr, unsigned char *write_buffer, int write_size,
+		unsigned int read_addr, unsigned char *read_buffer, int read_size);
+	void (*write_read_end)(int bus, int error, int result);
 } SceI2cDebugHandlers;
 
-int ksceI2cTransferRead(int bus, unsigned int address, unsigned char *buffer, int size);
-int ksceI2cTransferWrite(int bus, unsigned int device, const unsigned char *buffer, int size);
+int ksceI2cInit(int bus);
+int ksceI2cTransferRead(int bus, unsigned int addr, unsigned char *buffer, int size);
+int ksceI2cTransferWrite(int bus, unsigned int addr, const unsigned char *buffer, int size);
 int ksceI2cTransferWriteRead(int bus,
-	unsigned int write_device, const unsigned char *write_buffer, int write_size,
-	unsigned int read_device, unsigned char *read_buffer, int read_size);
+	unsigned int write_addr, unsigned char *write_buffer, int write_size,
+	unsigned int read_addr, unsigned char *read_buffer, int read_size);
 
 int ksceI2cSetDebugHandlers(int bus, SceI2cDebugHandlers *debug_handlers);
 
