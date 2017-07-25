a78f0794e7b150d16bf4b5aae25e840482dea53f
Travis CI <builds@travis-ci.com>
2017-07-25 10:21:39 +0000

    Generated documents from TravisCI for build 447

--- navtreedata.js
+++ navtreedata.js
@@ -22,11 +22,11 @@ var NAVTREEINDEX =
 "annotated.html",
 "group__SceCPUKernel.html#gacb2397373c553acca09aeb261d8e50c4",
 "group__SceCtrl.html",
-"group__SceFcntlKernel.html#gga5733272a008ed1a31fc1d94e7c42324fa30207bcde2a4a48be05f1dd2365492ec",
-"group__SceLoadcore.html",
-"group__SceRtcUser.html",
-"group__SceSystemParamUser.html#gga60caf27f6d446ccd1dfe165e1b6bee5eaf7ab2d73ba07983d7a60cc64a14807f5",
-"group__SceTouch.html"
+"group__SceDisplayUser.html#gab0dbd41679e2aab2325ceac6a41cc052",
+"group__SceGxtUser.html#ga14156f39d541149e445154fc7dc3718e",
+"group__ScePowerUser.html#gaea7881caee1ac4ea9fc0e25e5553dc51",
+"group__SceSuspend.html",
+"group__SceThreadMgrUser.html#ga7fa2373891f0175338c0c5975b371c1c"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
