91144e799cf3ca737065be5d9cee641dcb72bc48
Travis CI <builds@travis-ci.com>
2017-10-27 08:30:35 +0000

    Generated documents from TravisCI for build 559

--- group__SceSysmemKernel_structSceObjectBase.js
+++ group__SceSysmemKernel_structSceObjectBase.js
@@ -1,4 +1,5 @@
 var group__SceSysmemKernel_structSceObjectBase =
 [
+    [ "data", "group__SceSysmemKernel.html#ga088c0524231333a142c6f800ebadb537", null ],
     [ "sce_reserved", "group__SceSysmemKernel.html#ga194b74736629efb75abb894ca3d33dc3", null ]
 ];
