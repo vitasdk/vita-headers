c61e38e7d239a3f72e9e40aff2bec82aac0fb43b
Travis CI <builds@travis-ci.com>
2017-11-10 11:38:53 +0000

    Generated documents from TravisCI for build 580

---
+++ group__SceJpegEncUser_structSceJpegEncoderInitParamOption.js
@@ -0,0 +1,4 @@
+var group__SceJpegEncUser_structSceJpegEncoderInitParamOption =
+[
+    [ "SCE_JPEGENC_INIT_PARAM_OPTION_NONE", "group__SceJpegEncUser.html#ga8cfc4467dc9530cb2edfbce11aaab99b", null ]
+];
