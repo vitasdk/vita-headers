62e971bb7340bfecb986c2060bc002e41dfaaaa2
Sergi Granell <xerpi.g.12@gmail.com>
2017-11-30 10:40:59 +0100

    Add ksceKernelCpuGetCpuId NID

--- include/psp2kern/kernel/cpu.h
+++ include/psp2kern/kernel/cpu.h
@@ -18,29 +18,32 @@ extern "C" {
  *
  * @param      state  The state
  */
-#define ENTER_SYSCALL(state) do { \
-  asm volatile ("mrc p15, 0, %0, c13, c0, 3" : "=r" (state)); \
-  asm volatile ("mcr p15, 0, %0, c13, c0, 3" :: "r" (state << 16) : "memory"); \
-} while(0)
+#define ENTER_SYSCALL(state) \
+	do { \
+		asm volatile ("mrc p15, 0, %0, c13, c0, 3" : "=r" (state)); \
+		asm volatile ("mcr p15, 0, %0, c13, c0, 3" :: "r" (state << 16) : "memory"); \
+	} while(0)
 
 /**
  * @brief      Call this when existing a syscall
  *
  * @param      state  The state
  */
-#define EXIT_SYSCALL(state) do { \
-  asm volatile ("mcr p15, 0, %0, c13, c0, 3" :: "r" (state) : "memory"); \
-} while (0)
+#define EXIT_SYSCALL(state) \
+	do { \
+		asm volatile ("mcr p15, 0, %0, c13, c0, 3" :: "r" (state) : "memory"); \
+	} while (0)
 
 /**
  * @brief      Save process context
  *
  * @param      context  The context
  */
-static inline void ksceKernelCpuSaveContext(int context[3]) {
-  asm ("mrc p15, 0, %0, c2, c0, 1" : "=r" (context[0]));
-  asm ("mrc p15, 0, %0, c3, c0, 0" : "=r" (context[1]));
-  asm ("mrc p15, 0, %0, c13, c0, 1" : "=r" (context[2]));
+static inline void ksceKernelCpuSaveContext(int context[3])
+{
+	asm ("mrc p15, 0, %0, c2, c0, 1" : "=r" (context[0]));
+	asm ("mrc p15, 0, %0, c3, c0, 0" : "=r" (context[1]));
+	asm ("mrc p15, 0, %0, c13, c0, 1" : "=r" (context[2]));
 }
 
 /**
@@ -48,26 +51,31 @@ static inline void ksceKernelCpuSaveContext(int context[3]) {
  *
  * @param      context  The context, can be from ::ksceKernelGetPidContext
  */
-static inline void ksceKernelCpuRestoreContext(int context[3]) {
-  int cpsr;
-  int tmp;
+static inline void ksceKernelCpuRestoreContext(int context[3])
+{
+	int cpsr, tmp;
 
-  asm volatile ("mrs %0, cpsr" : "=r" (cpsr));
-  if (!(cpsr & 0x80)) {
-    asm volatile ("cpsid i" ::: "memory");
-  }
-  asm volatile ("mrc p15, 0, %0, c13, c0, 1" : "=r" (tmp));
-  tmp = (tmp & 0xFFFFFF00) | context[2];
-  asm volatile ("mcr p15, 0, %0, c13, c0, 1" :: "r" (0));
-  asm volatile ("isb" ::: "memory");
-  asm volatile ("mcr p15, 0, %0, c2, c0, 1" :: "r" (context[0] | 0x4A));
-  asm volatile ("isb" ::: "memory");
-  asm volatile ("mcr p15, 0, %0, c13, c0, 1" :: "r" (tmp));
-  asm volatile ("mcr p15, 0, %0, c3, c0, 0" :: "r" (context[1] & 0x55555555));
-  if (!(cpsr & 0x80)) {
-    asm volatile ("cpsie i" ::: "memory");
-  }
+	asm volatile ("mrs %0, cpsr" : "=r" (cpsr));
+	if (!(cpsr & 0x80))
+		asm volatile ("cpsid i" ::: "memory");
+	asm volatile ("mrc p15, 0, %0, c13, c0, 1" : "=r" (tmp));
+	tmp = (tmp & 0xFFFFFF00) | context[2];
+	asm volatile ("mcr p15, 0, %0, c13, c0, 1" :: "r" (0));
+	asm volatile ("isb" ::: "memory");
+	asm volatile ("mcr p15, 0, %0, c2, c0, 1" :: "r" (context[0] | 0x4A));
+	asm volatile ("isb" ::: "memory");
+	asm volatile ("mcr p15, 0, %0, c13, c0, 1" :: "r" (tmp));
+	asm volatile ("mcr p15, 0, %0, c3, c0, 0" :: "r" (context[1] & 0x55555555));
+	if (!(cpsr & 0x80))
+		asm volatile ("cpsie i" ::: "memory");
 }
+
+/**
+ * @brief      Returns the CPU ID of the calling processor
+ *
+ * @return     The CPU ID
+ */
+int ksceKernelCpuGetCpuId(void);
 
 /**
  * @brief      Disabled interrupts
