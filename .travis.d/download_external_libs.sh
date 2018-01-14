25873325f8346eaca8dca4d9fdab30138ef34ef9
Sunguk Lee <d3m3vilurr@gmail.com>
2018-01-14 20:00:21 +0900

    Apply vdpm changes
    vitasdk/vdpm#25 makes problems

--- .travis.d/download_external_libs.sh
+++ .travis.d/download_external_libs.sh
@@ -2,5 +2,7 @@
 set -e
 curl -sL https://github.com/vitasdk/vdpm/raw/master/vdpm -o vdpm
 chmod +x vdpm
-curl -L https://github.com/vitasdk/vdpm/raw/master/install-all.sh | bash > /dev/null
+curl -L https://github.com/vitasdk/vdpm/raw/master/include/install-packages.sh -o install-packages.sh
+sed -i "s/\.\.\///" install-packages.sh
+curl -L https://github.com/vitasdk/vdpm/raw/master/install-all.sh | sed -e "s/include\///" | bash > /dev/null
 rm vdpm
