1d813471cb91647f72fec88271099c2c10c57957
Travis CI <builds@travis-ci.com>
2018-02-10 14:17:12 +0000

    Generated documents from TravisCI for build 718

--- group__SceIntrMgrKernel_structSceKernelIntrOptParam.js
+++ group__SceIntrMgrKernel_structSceKernelIntrOptParam.js
@@ -1,7 +1,7 @@
 var group__SceIntrMgrKernel_structSceKernelIntrOptParam =
 [
+    [ "handlers", "group__SceIntrMgrKernel.html#gab1664298a6191eb3fa2328b2cbb253f9", null ],
     [ "num", "group__SceIntrMgrKernel.html#gadc6fde638d1c2eef7e334288b99197b3", null ],
-    [ "opt2", "group__SceIntrMgrKernel.html#ga4049bb4c3600535b75bf6d3d3a8bca41", null ],
     [ "size", "group__SceIntrMgrKernel.html#ga1c8c282753748e588d4cb9176c7f1a72", null ],
     [ "unk_10", "group__SceIntrMgrKernel.html#gab0038f00f3a72fae53493048a507c9e5", null ],
     [ "unk_C", "group__SceIntrMgrKernel.html#ga018a2789b592a9b8a521f2b1ede8daac", null ]
