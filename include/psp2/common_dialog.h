6ca6585bc425f0d751e130c9bad858213cac8ddc
Francisco José García García <frangarcj@gmail.com>
2018-04-21 13:21:30 +0200

    Workaround for non 64-bit systems

--- include/psp2/common_dialog.h
+++ include/psp2/common_dialog.h
@@ -107,7 +107,9 @@ typedef struct SceCommonDialogParam {
 static inline
 void _sceCommonDialogSetMagicNumber(SceCommonDialogParam *param)
 {
+#ifdef __vita__
 	param->magic = SCE_COMMON_DIALOG_MAGIC_NUMBER + (SceUInt32)param;
+#endif
 }
 
 static inline
