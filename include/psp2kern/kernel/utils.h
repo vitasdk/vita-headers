f777b5a9fa2dc99c7956edff09444469d7c7621b
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2018-08-30 01:56:03 +0900

    Add psp2kern/kernel/utils.h (#349)

---
+++ include/psp2kern/kernel/utils.h
@@ -0,0 +1,73 @@
+/**
+ * \kernelgroup{SceUtils}
+ * \usage{psp2kern/kernel/utils.h,SceKernelUtilsForDriver_stub}
+ */
+
+
+#ifndef _PSP2_KERNEL_UTILS_H_
+#define _PSP2_KERNEL_UTILS_H_
+
+#include <psp2kern/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SCE_SHA1_BLOCK_SIZE    64
+#define SCE_SHA1_DIGEST_SIZE   20
+#define SCE_SHA224_BLOCK_SIZE  64
+#define SCE_SHA224_DIGEST_SIZE 28
+#define SCE_SHA256_BLOCK_SIZE  64
+#define SCE_SHA256_DIGEST_SIZE 32
+
+typedef struct SceSha1Context {
+	uint32_t h[5];
+	uint16_t usRemains;
+	uint16_t usComputed;
+	uint64_t ullTotalLen;
+	char buf[SCE_SHA1_BLOCK_SIZE];
+	char result[SCE_SHA1_DIGEST_SIZE];
+	uint32_t pad;
+} SceSha1Context;
+
+typedef struct SceSha224Context {
+	uint32_t h[8];
+	uint32_t pad;
+	uint16_t usRemains;
+	uint16_t usComputed;
+	uint64_t ullTotalLen;
+	char buf[SCE_SHA224_BLOCK_SIZE];
+	char result[SCE_SHA224_DIGEST_SIZE];
+	uint32_t pad2;
+} SceSha224Context;
+
+typedef struct SceSha256Context {
+	uint32_t h[8];
+	uint32_t pad;
+	uint16_t usRemains;
+	uint16_t usComputed;
+	uint64_t ullTotalLen;
+	char buf[SCE_SHA256_BLOCK_SIZE];
+	char result[SCE_SHA256_DIGEST_SIZE];
+} SceSha256Context;
+
+int ksceSha1BlockInit(SceSha1Context *pContext);
+int ksceSha1BlockUpdate(SceSha1Context *pContext, const void *plain, uint32_t len);
+int ksceSha1BlockResult(SceSha1Context *pContext, char *digest);
+int ksceSha1Digest(const void *plain, uint32_t len, char *result);
+
+int ksceSha224BlockInit(SceSha224Context *pContext);
+int ksceSha224BlockUpdate(SceSha224Context *pContext, const void *plain, uint32_t len);
+int ksceSha224BlockResult(SceSha224Context *pContext, char *digest);
+int ksceSha224Digest(const void *plain, uint32_t len, char *result);
+
+int ksceSha256BlockInit(SceSha256Context *pContext);
+int ksceSha256BlockUpdate(SceSha256Context *pContext, const void *plain, uint32_t len);
+int ksceSha256BlockResult(SceSha256Context *pContext, char *digest);
+int ksceSha256Digest(const void *plain, uint32_t len, char *result);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP2_KERNEL_UTILS_H_ */
