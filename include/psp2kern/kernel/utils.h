64914db8e6adbb8c7b8bf098c4108585728075c8
Sunguk Lee <d3m3vilurr@gmail.com>
2018-10-04 05:33:28 +0900

    psp2kern: Change args types of ksceGzipDecompress (#354)
    these value should be uint32_t instead signed int
    related: #353 comments

--- include/psp2kern/kernel/utils.h
+++ include/psp2kern/kernel/utils.h
@@ -78,7 +78,7 @@ int ksceHmacSha256Digest(const unsigned char *key, uint32_t key_len, const void 
  *
  * @return dst_size on success, < 0 on error.
  */
-int ksceGzipDecompress(void *dst, int dst_size, const void *src, int *crc32);
+int ksceGzipDecompress(void *dst, uint32_t dst_size, const void *src, uint32_t *crc32);
 
 #ifdef __cplusplus
 }
