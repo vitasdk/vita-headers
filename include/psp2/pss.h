3b1ed5122f182dab151d0a3ba27dc5c85134f430
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2018-10-21 09:09:02 +1300

    Update pss.h
    It appears that it's 1 whenever it's called it's also only ever used when PSM games are starting from what I can see :?

--- include/psp2/pss.h
+++ include/psp2/pss.h
@@ -13,15 +13,15 @@
 extern "C" {
 #endif
   
-typedef struct ScePssHandle {
+typedef struct ScePssCryptoHandle {
   uint32_t unk0;
   uint32_t unk1;
-  uint32_t filesz;
+  SceSize size;
   uint32_t unk3;
 } ScePssCryptoHandle;
 
 int pss_crypto_open_p(ScePssCryptoHandle *handle, char *path);
-char *pss_crypto_read_p(ScePssCryptoHandle *handle, int unk);
+char *pss_crypto_read_p(ScePssCryptoHandle *handle, int *mode);
 void *pss_code_mem_alloc(SceSize *);
 void pss_code_mem_flush_icache(const void *, SceSize);
 void pss_code_mem_lock(void);
