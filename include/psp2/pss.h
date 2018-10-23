06c777d28168af495b73caf4b22e94e5d158272a
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2018-10-24 10:41:01 +1300

    Update pss.h (#356)
    Update pss.h

--- include/psp2/pss.h
+++ include/psp2/pss.h
@@ -12,7 +12,16 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+  
+typedef struct ScePssCryptoHandle {
+  uint32_t unk0;
+  uint32_t unk1;
+  SceSize size;
+  uint32_t unk3;
+} ScePssCryptoHandle;
 
+int pss_crypto_open_p(ScePssCryptoHandle *handle, char *path);
+char *pss_crypto_read_p(ScePssCryptoHandle *handle, int *mode);
 void *pss_code_mem_alloc(SceSize *);
 void pss_code_mem_flush_icache(const void *, SceSize);
 void pss_code_mem_lock(void);
