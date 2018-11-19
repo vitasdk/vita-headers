ac783c0095470f36acac5a06fa261ab4c406a1a9
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2018-11-19 22:26:34 +0900

    Add ksceNetConnect

--- include/psp2kern/net/net.h
+++ include/psp2kern/net/net.h
@@ -545,6 +545,7 @@ typedef struct SceNetIcmpHeader {
 int ksceNetSocket(const char *name, int domain, int type, int protocol);
 int ksceNetAccept(int s, SceNetSockaddr *addr, unsigned int *addrlen);
 int ksceNetBind(int s, const SceNetSockaddr *addr, unsigned int addrlen);
+int ksceNetConnect(int s, const SceNetSockaddr *name, unsigned int namelen);
 int ksceNetListen(int s, int backlog);
 int ksceNetRecvfrom(int s, void *buf, unsigned int len, int flags, SceNetSockaddr *from, unsigned int *fromlen);
 int ksceNetSendto(int s, const void *msg, unsigned int len, int flags, const SceNetSockaddr *to, unsigned int tolen);
