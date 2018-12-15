9e740d05d8ac4f3efff93bcfe46c5dd5d3cdb6fc
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2018-12-15 18:16:30 +0900

    Update vshbridge.h

--- include/psp2/vshbridge.h
+++ include/psp2/vshbridge.h
@@ -14,7 +14,7 @@ extern "C" {
 #endif
 
 int _vshSblGetSystemSwVersion(SceKernelFwInfo * data);
- 
+
 int _vshSblAimgrGetConsoleId(char CID[32]);
 
 /**
@@ -40,7 +40,17 @@ int vshIoUmount(int id, int unk1, int unk2, int unk3);
 int vshIdStorageIsDirty(void);
 int vshIdStorageIsFormatted(void);
 int vshIdStorageIsReadOnly(void);
- 
+
+/**
+ * @param[in] leafnum - 0x0 ~ 0x80 / leafnum > 0x80 = error
+ * @param[out] buf - Leaf data
+ * @param[in] buf_size - set 0x200 / buf_size != 0x200 = 0x800F090D
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int vshIdStorageReadLeaf(int leafnum, void *buf, int buf_size);
+int vshIdStorageWriteLeaf(int leafnum, const void *buf, int buf_size);
+
 int vshSblAimgrIsCEX(void);
 int vshSblAimgrIsDEX(void);
 int vshSblAimgrIsGenuineVITA(void);
@@ -48,15 +58,20 @@ int vshSblAimgrIsGenuineDolce(void);
 int vshSblAimgrIsTest(void);
 int vshSblAimgrIsTool(void);
 int vshSblSsIsDevelopmentMode(void);
-int sceKernelIsPSVitaTV(void);
- 
+
 int vshSysconHasWWAN(void);
- 
+
 int vshSysconIsDownLoaderMode(void);
 int vshSysconIsIduMode(void);
 int vshSysconIsMCEmuCapable(void);
 int vshSysconIsShowMode(void);
- 
+
+int vshSysconIduModeSet(void);
+int vshSysconIduModeClear(void);
+
+int vshSysconShowModeSet(void);
+int vshSysconShowModeClear(void);
+
 int vshMemoryCardGetCardInsertState(void);
 int vshRemovableMemoryGetCardInsertState(void);
 
