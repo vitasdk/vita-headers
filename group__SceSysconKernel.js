ff59440cbefd9912c8782b5033ec53e7f5bdc03e
Travis CI <builds@travis-ci.com>
2019-01-28 04:47:16 +0000

    Generated documents from TravisCI for build 1050

--- group__SceSysconKernel.js
+++ group__SceSysconKernel.js
@@ -5,14 +5,14 @@ var group__SceSysconKernel =
     [ "ksceSysconCmdSync", "group__SceSysconKernel.html#ga45f5b6a1ff38625a49fa539e53d8c0db", null ],
     [ "ksceSysconCtrlHdmiCecPower", "group__SceSysconKernel.html#gad9d6268ecf056da13880be8679d6a47f", null ],
     [ "ksceSysconCtrlLED", "group__SceSysconKernel.html#ga1e4c75735921c15791258dffa9503d89", null ],
-    [ "ksceSysconCtrlMsPower", "group__SceSysconKernel.html#ga0b612f32286c61b7ac4bb33b38608917", null ],
+    [ "ksceSysconCtrlRMRPower", "group__SceSysconKernel.html#ga5a73cfa36fb2601d2a91e122ff0420a3", null ],
     [ "ksceSysconCtrlSdPower", "group__SceSysconKernel.html#ga3c93408fa2ce44f21ccda85f76db0d4b", null ],
     [ "ksceSysconGetBaryonTimestamp", "group__SceSysconKernel.html#ga74b11bfee0b45aefe0c8349016d6844d", null ],
     [ "ksceSysconGetBaryonVersion", "group__SceSysconKernel.html#ga6e252e6677e8635292642e30fdddac78", null ],
-    [ "ksceSysconResetDevice", "group__SceSysconKernel.html#ga7aea3f11068c78ae7149d1fdf03c231f", null ],
     [ "ksceSysconSetAlarmCallback", "group__SceSysconKernel.html#ga709e091a8a156e457907af9606c84d40", null ],
     [ "ksceSysconSetDebugHandlers", "group__SceSysconKernel.html#ga0c7b30cee806394963eea6190bc205fd", null ],
     [ "ksceSysconSetLowBatteryCallback", "group__SceSysconKernel.html#ga19209e2b0ee8adf1a05e983e45e6271a", null ],
+    [ "ksceSysconSetPowerMode", "group__SceSysconKernel.html#gaef0ddcbf2f42313ac8b539d3e146d7d6", null ],
     [ "ksceSysconSetThermalAlertCallback", "group__SceSysconKernel.html#gaec856771758bff4fceda58297c2b7f10", null ],
     [ "ksceSysconWaitInitialized", "group__SceSysconKernel.html#ga3f3e840a1dfb319aa9018867ec1147fd", null ]
 ];
