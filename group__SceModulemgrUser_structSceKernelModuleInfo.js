fe526841aa9800b3f6570f87b960116fa9723478
Travis CI <builds@travis-ci.com>
2019-12-16 14:55:55 +0000

    Generated documents from TravisCI for build 1194

--- group__SceModulemgrUser_structSceKernelModuleInfo.js
+++ group__SceModulemgrUser_structSceKernelModuleInfo.js
@@ -4,6 +4,7 @@ var group__SceModulemgrUser_structSceKernelModuleInfo =
     [ "exidxTop", "group__SceModulemgrUser.html#ga6756078fd0f270e512f24d1affcb097f", null ],
     [ "flags", "group__SceModulemgrUser.html#ga7141a686765dea8fb44ac3d29c7b9546", null ],
     [ "handle", "group__SceModulemgrUser.html#ga517048842f3ca8d2ddfac00186b86d43", null ],
+    [ "module_exit", "group__SceModulemgrUser.html#gaba075df032e5903475749df3c5c7e7cc", null ],
     [ "module_name", "group__SceModulemgrUser.html#ga78e7870cf88593b5d2b74058a85623fc", null ],
     [ "module_start", "group__SceModulemgrUser.html#ga56f1d33ef08630348ca0220ee78d6fb6", null ],
     [ "module_stop", "group__SceModulemgrUser.html#ga744c40502c5fb55f1a1d4da9c96dd3d3", null ],
@@ -15,7 +16,6 @@ var group__SceModulemgrUser_structSceKernelModuleInfo =
     [ "tlsInitSize", "group__SceModulemgrUser.html#gaed40e435b4a86ba36ae988d15886d26b", null ],
     [ "type", "group__SceModulemgrUser.html#ga244ee8ce4e879128ca9eae64dbcac33b", null ],
     [ "unk28", "group__SceModulemgrUser.html#ga79d060d0cc315d567044f591b13ff8bb", null ],
-    [ "unk30", "group__SceModulemgrUser.html#ga7d7d4e9ec051452515cff5eb28ff6c69", null ],
     [ "unk40", "group__SceModulemgrUser.html#gaf5cce4425b8b073fc627f1d6018fe068", null ],
     [ "unk44", "group__SceModulemgrUser.html#gae59b2785c09774a61d5c2b8262503bef", null ]
 ];
