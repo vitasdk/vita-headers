63f5a0711ecc08d9ab9018ffbd9c7dba5a56b8e8
Bluzume <39113159+KuromeSan@users.noreply.github.com>
2019-12-27 11:44:07 +1300

    Match db.yml names (pss_crypto_open)
    the _p was from a plugin that hooks these functions ..

--- include/psp2/pss.h
+++ include/psp2/pss.h
@@ -20,8 +20,8 @@ typedef struct ScePssCryptoHandle {
   uint32_t unk3;
 } ScePssCryptoHandle;
 
-int pss_crypto_open_p(ScePssCryptoHandle *handle, char *path);
-char *pss_crypto_read_p(ScePssCryptoHandle *handle, int *mode);
+int pss_crypto_open(ScePssCryptoHandle *handle, char *path);
+char *pss_crypto_read(ScePssCryptoHandle *handle, int *mode);
 void *pss_code_mem_alloc(SceSize *);
 void pss_code_mem_flush_icache(const void *, SceSize);
 void pss_code_mem_lock(void);
