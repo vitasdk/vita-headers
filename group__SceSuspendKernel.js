95489a584a22ebacecb0fc8a0fe53b889e0b322c
Travis CI <builds@travis-ci.com>
2020-03-07 21:05:14 +0000

    Generated documents from TravisCI for build 1228

--- group__SceSuspendKernel.js
+++ group__SceSuspendKernel.js
@@ -1,5 +1,6 @@
 var group__SceSuspendKernel =
 [
+    [ "SceSysEventHandler", "group__SceSuspendKernel.html#ga7d6547b2c52292c1f1b9bd0e001ac782", null ],
     [ "SceKernelPowerTickType", "group__SceSuspendKernel.html#ga1b448620c7525b730a2b94164e34d77f", [
       [ "SCE_KERNEL_POWER_TICK_DEFAULT", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fae71baa308d1cf3469b372f41a74de0e9", null ],
       [ "SCE_KERNEL_POWER_TICK_DISABLE_AUTO_SUSPEND", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fa3e8b9af3aaeb7cd8fb6ce66a8063d872", null ],
@@ -14,5 +15,6 @@ var group__SceSuspendKernel =
     [ "SCE_KERNEL_POWER_TICK_DISABLE_AUTO_SUSPEND", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fa3e8b9af3aaeb7cd8fb6ce66a8063d872", null ],
     [ "SCE_KERNEL_POWER_TICK_DISABLE_OLED_DIMMING", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fa55cba5df6d5a94e9b0188c47696cb650", null ],
     [ "SCE_KERNEL_POWER_TICK_DISABLE_OLED_OFF", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fa61d6ff07f428ecf1f42ecb4644002d39", null ],
-    [ "ksceKernelPowerTick", "group__SceSuspendKernel.html#ga6c15db71646bdff1e4fd4ff45c71bcc4", null ]
+    [ "ksceKernelPowerTick", "group__SceSuspendKernel.html#ga6c15db71646bdff1e4fd4ff45c71bcc4", null ],
+    [ "ksceKernelRegisterSysEventHandler", "group__SceSuspendKernel.html#gaa7f1736fd9b7b9d3473d81573a3e875f", null ]
 ];
---
+++ navtreeindex73.js
@@ -0,0 +1,4 @@
+var NAVTREEINDEX73 =
+{
+"vshbridge_8h_source.html":[3,0,1,0,57]
+};
