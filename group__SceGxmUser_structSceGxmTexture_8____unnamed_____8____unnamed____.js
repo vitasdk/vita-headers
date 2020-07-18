2c6d216fb9b756c84254682d9e46cb92cee518c6
Travis CI <builds@travis-ci.com>
2020-07-18 14:55:19 +0000

    Generated documents from TravisCI for build 1303

--- group__SceGxmUser_structSceGxmTexture_8____unnamed_____8____unnamed____.js
+++ group__SceGxmUser_structSceGxmTexture_8____unnamed_____8____unnamed____.js
@@ -1,5 +1,5 @@
 var group__SceGxmUser_structSceGxmTexture_8____unnamed_____8____unnamed____ =
 [
-    [ "mip_filter", "group__SceGxmUser.html#aefc004b8bc9ada9e4e9631f40c1b6f64", null ],
-    [ "min_filter", "group__SceGxmUser.html#a4d37edc105deb9b1f274a49a74967e20", null ]
+    [ "mip_count", "group__SceGxmUser.html#a33151bb0277064702feef0b96b3ce3fd", null ],
+    [ "lod_bias", "group__SceGxmUser.html#a08b979a47bdfc6534264edf848d229c5", null ]
 ];
