170194f94dc0438f5fef2d9eade27cb618e53a75
bythos14 <45720552+bythos14@users.noreply.github.com>
2020-09-21 06:57:49 -0400

    Added SceBgAppUtil, SceNotificationUtil, SceTriggerUtil and SceIncomingDialog libraries
    Full credit to @GrapheneCt
    
    Original headers can be found at https://github.com/GrapheneCt/Vita-PoC
    
    ----
    [license approval][1]
    ```
    @GrapheneCt
    
    You can add headers contained in this repository to vita-headers under
    the following conditions:
    
        Credit me (link this repository)
        This permission is only valid for header files (.h) and not for the
    code PoC files (.c)
        This permission is only valid for current (20.09.20) state of this
    repository. All future commits are not permissioned unless stated
    otherwise in the future.
        As for the NIDs, you can copy NIDs for following libraries from
    DolceSDK: SceNotificationUtil, SceBgAppUtil. All other NIDs are
    contained in headers of this repository as comments after functions.
    ```
    
    [1]: https://github.com/GrapheneCt/Vita-PoC/issues/1#issuecomment-695832755
    
    Signed-off-by: Sunguk Lee <d3m3vilurr@gmail.com>

---
+++ include/psp2/notificationutil.h
@@ -0,0 +1,92 @@
+﻿/**
+ * \usergroup{SceNotificationUtil}
+ * \usage{psp2/notificationutil.h,SceNotificationUtil_stub,SCE_SYSMODULE_NOTIFICATION_UTIL}
+ */
+
+#ifndef _PSP2_NOTIFICATIONUTIL_H_
+#define _PSP2_NOTIFICATIONUTIL_H_
+
+#include <psp2/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Error Codes
+ */
+typedef enum SceNotificationUitlErrorCode {
+	SCE_NOTIFICATIONUTIL_ERROR_INTERNAL = 0x80106300
+} SceNotificationUtilErrorCode;
+
+/**
+ * BGDL-type notification event handler function
+ */
+typedef void(*SceNotificationUtilProgressEventHandler)(int eventId);
+
+typedef struct SceNotificationUtilProgressInitParam {
+	SceWChar16 notificationText[0x3F];
+	SceInt16 separator0;						//must be 0
+	SceWChar16 notificationSubText[0x3F];
+	SceInt16 separator1;						//must be 0
+	SceChar8 unk[0x3E6];
+	SceInt32 unk_4EC;						//can be set to 0
+	SceNotificationUtilProgressEventHandler eventHandler;
+} SceNotificationUtilProgressInitParam;
+
+typedef struct SceNotificationUtilProgressUpdateParam {
+	SceWChar16 notificationText[0x3F];
+	SceInt16 separator0;						//must be 0
+	SceWChar16 notificationSubText[0x3F];
+	SceInt16 separator1;						//must be 0
+	SceFloat targetProgress;
+	SceChar8 reserved[0x38];
+} SceNotificationUtilProgressUpdateParam;
+
+typedef struct SceNotificationUtilProgressFinishParam {
+	SceWChar16 notificationText[0x3F];
+	SceInt16 separator0;						//must be 0
+	SceWChar16 notificationSubText[0x3F];
+	SceInt16 separator1;						//must be 0
+	SceChar8 path[0x3E8];
+} SceNotificationUtilProgressFinishParam;
+
+/**
+ * Initialize notification util for use with BG application.
+ *
+ * Does not need to be called for normal applications.
+ */
+SceInt32 sceNotificationUtilBgAppInitialize(void);
+
+/**
+ * Send notification.
+ *
+ * Text buffer size must be 0x410.
+ */
+SceInt32 sceNotificationUtilSendNotification(const SceWChar16* text);
+
+/**
+ * Clean notifications for calling app from notification history.
+ */
+SceInt32 sceNotificationUtilCleanHistory(void);
+
+/**
+ * Start BGDL-type notification.
+ */
+SceInt32 sceNotificationUtilProgressBegin(SceNotificationUtilProgressInitParam* initParams);
+
+/**
+ * Update BGDL-type notification.
+ */
+SceInt32 sceNotificationUtilProgressUpdate(SceNotificationUtilProgressUpdateParam* updateParams);
+
+/**
+ * Finish BGDL-type notification.
+ */
+SceInt32 sceNotificationUtilProgressFinish(SceNotificationUtilProgressFinishParam* finishParams);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* _PSP2_NOTIFICATIONUTIL_H_ */
