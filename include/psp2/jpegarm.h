fe43e4335f6c94015797462cae50e8e44c36dc28
bythos14 <45720552+bythos14@users.noreply.github.com>
2020-09-24 05:47:16 -0400

    Added SceJpegArm
    Full credit to @GrapheneCt
    
    Original header can be found at https://github.com/GrapheneCt/Vita-PoC
    
    Added usage alias to Doxyfile for internal sysmodules.
    
    ----
    [license approval][1]
    ```
    @GrapheneCt
    
    You can add headers contained in this repository to vita-headers under
    the following conditions:
    
        Credit me (link this repository)
        This permission is only valid for header files (.h) and not for the
    code PoC files (.c)
        This permission is only valid for current (20.09.20) state of this
    repository. All future commits are not permissioned unless stated
    otherwise in the future.
        As for the NIDs, you can copy NIDs for following libraries from
    DolceSDK: SceNotificationUtil, SceBgAppUtil. All other NIDs are
    contained in headers of this repository as comments after functions.
    ```
    
    [1]: https://github.com/GrapheneCt/Vita-PoC/issues/1#issuecomment-695832755
    
    Signed-off-by: Sunguk Lee <d3m3vilurr@gmail.com>

---
+++ include/psp2/jpegarm.h
@@ -0,0 +1,41 @@
+/**
+ * \usergroup{SceJpegArm}
+ * \internalusage{psp2/jpegarm.h,SceJpegArm_stub,SCE_SYSMODULE_INTERNAL_JPEG_ARM}
+ */
+
+#ifndef _PSP2_JPEGARM_H_
+#define _PSP2_JPEGARM_H_
+
+#include <psp2/types.h>
+#include <psp2/jpeg.h>
+
+/**
+ * Error Codes
+ */
+typedef enum SceJpegArmErrorCode {
+    SCE_JPEG_ARM_OK = 0 
+} SceJpegArmErrorCode;
+
+int sceJpegArmDecodeMJpeg(const SceUInt8* pJpeg,
+                          SceSize isize,
+                          SceInt decodeMode,
+                          void* pRGBA,
+                          SceSize osize,
+                          void* pCoefBuffer,
+                          SceSize coefBufferSize);
+
+int sceJpegArmDecodeMJpegYCbCr(const SceUInt8* pJpeg,
+                               SceSize isize,
+                               SceInt decodeMode,
+                               SceUInt8* pYCbCr,
+                               SceSize osize,
+                               void* pCoefBuffer,
+                               SceSize coefBufferSize);
+
+int sceJpegArmGetOutputInfo(const SceUInt8* pJpeg,
+                            SceSize isize,
+                            SceInt decodeMode,
+                            SceInt outputFormat,
+                            SceJpegOutputInfo* pOutputInfo);
+
+#endif /* _PSP2_JPEGARM_H_ */
