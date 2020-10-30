364a3f5313d55ff0f6c3e00ae8a81aafef190650
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-10-30 22:11:40 +0900

    Update SceSblSsUpdateMgr (#491)
    * Update SceSblSsUpdateMgr
    
    * Create update.h
    
    * Add SceUpdateMgr
    
    * Fix wrong path
    
    * Update function description
    
    * Fix param out
    
    * Remove postfix
    
    * Remove postfix

---
+++ include/psp2/update.h
@@ -0,0 +1,45 @@
+/**
+ * \usergroup{SceUpdateMgr}
+ * \usage{psp2/update.h,SceSblSsUpdateMgr_stub}
+ */
+
+#ifndef _PSP2_UPDATE_MGR_H_
+#define _PSP2_UPDATE_MGR_H_
+
+#include <psp2/types.h>
+
+typedef char SceUpdateMode;
+
+#define SCE_UPDATE_MODE_SWU_GUI 0x10
+#define SCE_UPDATE_MODE_SWU_CUI 0x30
+
+/**
+ * Getting system update mode on boot
+ *
+ * @param[out] mode - The pointer of SceUpdateMode variable
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int sceSblUsGetUpdateMode(SceUpdateMode *mode);
+
+/**
+ * Setting system update mode on boot
+ *
+ * @param[in] mode - The update mode
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int sceSblUsSetUpdateMode(SceUpdateMode mode);
+
+/**
+ * Verify PUP
+ *
+ * @param[in] path - The PUP path
+ *
+ * @return 0 on success, < 0 on error.
+ *
+ * note - If verify CEX PUP on Devkit system, got error.
+ */
+int sceSblUsVerifyPup(const char *path);
+
+#endif /* _PSP2_UPDATE_MGR_H_ */
