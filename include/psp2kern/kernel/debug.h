ad0a62fe8f484dbdc83b9115c24a4156696f384e
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-10-31 18:08:35 +0900

    Update SceDebug (#496)
    * Add SceKernelDebugPrintFlags
    
    * Add ksceDebugPrintfs document
    
    * Add SceDbgLogLevel
    
    * Fix args
    
    * Add KernelAssertion document
    
    * Add SceDebug nids
    
    * Add SceDebug functions
    
    * Update SceKernelDebugMessageContext
    
    * Optimize document

--- include/psp2kern/kernel/debug.h
+++ include/psp2kern/kernel/debug.h
@@ -15,31 +15,88 @@
 extern "C" {
 #endif
 
-int ksceDebugPutchar(int character);
-
-int ksceDebugPrintf(const char *fmt, ...);
-
 typedef struct SceKernelDebugMessageContext {
   SceUInt32 hex_value0_hi;
   SceUInt32 hex_value0_lo;
   SceUInt32 hex_value1;
-  const char *msg0;
-  SceSize num;
-  const char *msg1;
+  const char *func;
+  SceSize line;
+  const char *file;
 } SceKernelDebugMessageContext;
 
-// msg_type_flag : 0 or 0xB
+typedef enum SceKernelDebugPrintFlags {
+  SCE_DBG_PRINT_FLAG_NONE = 0,
+  SCE_DBG_PRINT_FLAG_CORE = 1,
+  SCE_DBG_PRINT_FLAG_FUNC = 2,
+  SCE_DBG_PRINT_FLAG_LINE = 4,
+  SCE_DBG_PRINT_FLAG_FILE = 8
+} SceKernelDebugPrintFlags;
 
-int ksceDebugPrintf2(int msg_type_flag, const SceKernelDebugMessageContext *ctx, const char *fmt, ...);
+typedef enum SceDbgLogLevel {
+  SCE_DBG_LOG_LEVEL_TRACE = 0, /* An extremely verbose logging level, mostly useful for internal developers. */
+  SCE_DBG_LOG_LEVEL_DEBUG,     /* A diagnostic logging level. */
+  SCE_DBG_LOG_LEVEL_INFO,      /* An informational logging level. */
+  SCE_DBG_LOG_LEVEL_WARNING,   /* A logging level that gives warnings of situations detrimental to proper execution. */
+  SCE_DBG_LOG_LEVEL_ERROR,     /* A logging level that will report erroneous conditions in execution. */
+  SCE_DBG_NUM_LOG_LEVELS       /* The number of logging levels available. */
+} SceDbgLogLevel;
+
+int ksceDebugPutchar(int character);
+
+/**
+ * @brief Print log
+ *
+ * @param[in] fmt - print fmt
+ *
+ * @return 0 on success, < 0 on error.
+ *
+ * @note - log is pass to ksceDebugRegisterPutcharHandler's handler.
+ */
+int ksceDebugPrintf(const char *fmt, ...);
+
+/**
+ * @brief Print log with ctx
+ *
+ * @param[in] flags - ctx print flags, see:SceKernelDebugPrintFlags
+ * @param[in] ctx   - debug msg ctx
+ * @param[in] fmt   - print fmt
+ *
+ * @return 0 on success, < 0 on error.
+ *
+ * @note - main log is pass to ksceDebugRegisterPutcharHandler's handler.
+ *         ctx  log is pass to ksceDebugSetHandlers's handler.
+ */
+int ksceDebugPrintf2(int flags, const SceKernelDebugMessageContext *ctx, const char *fmt, ...);
 
 __attribute__((__noreturn__))
-void ksceDebugPrintKernelPanic(const SceKernelDebugMessageContext *ctx, void *some_address);
+void ksceDebugPrintKernelPanic(const SceKernelDebugMessageContext *ctx, const void *lr);
 
 __attribute__((__noreturn__))
-void ksceDebugPrintfKernelPanic(const SceKernelDebugMessageContext *ctx, void *some_address, const char *fmt, ...);
+void ksceDebugPrintfKernelPanic(const SceKernelDebugMessageContext *ctx, const void *lr, const char *fmt, ...);
 
-int ksceDebugPrintKernelAssertion(int condition, const SceKernelDebugMessageContext *ctx, void *some_address);
-int ksceDebugPrintfKernelAssertion(int unk, int condition, const SceKernelDebugMessageContext *ctx, int some_address, const char *fmt, ...);
+/**
+ * @brief Assertion with ctx
+ *
+ * @param[in] condition - condition
+ * @param[in] ctx       - debug msg ctx
+ * @param[in] lr        - The link register
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int ksceDebugPrintKernelAssertion(int condition, const SceKernelDebugMessageContext *ctx, const void *lr);
+
+/**
+ * @brief Assertion with ctx and Print
+ *
+ * @param[in] level     - print level, see:SceDbgLogLevel
+ * @param[in] condition - condition
+ * @param[in] ctx       - debug msg ctx
+ * @param[in] lr        - The link register
+ * @param[in] fmt       - print fmt
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int ksceDebugPrintfKernelAssertion(int level, int condition, const SceKernelDebugMessageContext *ctx, const void *lr, const char *fmt, ...);
 
 int ksceDebugSetHandlers(int (* func)(int unk, const char *format, const va_list args), void *args);
 
@@ -48,6 +105,22 @@ int ksceDebugRegisterPutcharHandler(int (* func)(void *args, char c), void *args
 void *ksceDebugGetPutcharHandler(void);
 
 int ksceDebugDisableInfoDump(int flag);
+
+/**
+ * @brief Get current minimum assertion level
+ *
+ * @return current minimum assertion level.
+ */
+int ksceKernelGetMinimumAssertionLevel(void);
+
+/**
+ * @brief Get current minimum assertion level
+ *
+ * @param[in] level - new minimum assertion level
+ *
+ * @return previous minimum assertion level.
+ */
+int ksceKernelSetMinimumAssertionLevel(int level);
 
 #ifdef __cplusplus
 }
