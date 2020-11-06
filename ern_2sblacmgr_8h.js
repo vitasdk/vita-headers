8f0cad063b4e54ae9d36b15ed9d7eeabae5d4e0b
Travis CI <builds@travis-ci.com>
2020-11-06 04:55:28 +0000

    Generated documents from TravisCI for build 1455

--- ern_2sblacmgr_8h.js
+++ ern_2sblacmgr_8h.js
@@ -1,7 +1,14 @@
 var ern_2sblacmgr_8h =
 [
+    [ "ksceSblACMgrIsRootProgram", "group__SceSblACMgrKernel.html#gad62e5289d1303bfdb808e60fbb323142", null ],
+    [ "ksceSblACMgrIsSystemProgram", "group__SceSblACMgrKernel.html#ga54a0e36f6417238c971e0be9048ac322", null ],
+    [ "ksceSblACMgrIsSystem", "group__SceSblACMgrKernel.html#gace43491d154618381fcb8ec2a4e20293", null ],
     [ "ksceSblACMgrIsShell", "group__SceSblACMgrKernel.html#ga9fd3271227fb64c6394b8b1303c3b2b9", null ],
     [ "ksceSblACMgrIsGameProgram", "group__SceSblACMgrKernel.html#gac04816b1e6acb09d10169a7cb5647212", null ],
     [ "ksceSblACMgrIsNonGameProgram", "group__SceSblACMgrKernel.html#ga74ec11cb22de63bfc7fa12f6a08a26cd", null ],
+    [ "ksceSblACMgrIsPspEmu", "group__SceSblACMgrKernel.html#ga6eee8750351af5b927950362d45dd08f", null ],
+    [ "ksceSblACMgrIsSceShell", "group__SceSblACMgrKernel.html#ga56d88eec80bba6ed76bb612019b3eb64", null ],
+    [ "ksceSblACMgrIsFself", "group__SceSblACMgrKernel.html#ga8e8b9a7255c43fd70d852e3af7cec215", null ],
+    [ "ksceSblACMgrGetProcessProgramAuthId", "group__SceSblACMgrKernel.html#ga864b72ba653d391cd41fc2b1e5f9b9e2", null ],
     [ "ksceSblACMgrIsDevelopmentMode", "group__SceSblACMgrKernel.html#ga9731aeaeff29c8e605b891046e6c5d78", null ]
 ];
