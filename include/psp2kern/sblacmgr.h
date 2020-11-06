cdfd500b20d4666f61c63394d6c7a194f3081874
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-11-06 11:08:44 +0900

    psp2kern: Update SceSblACMgrForDriver functions and documents (#501)
    * Fix wrong name
    
    * Add functions and documents
    
    * Update documents

--- include/psp2kern/sblacmgr.h
+++ include/psp2kern/sblacmgr.h
@@ -3,7 +3,6 @@
  * \usage{psp2kern/sblacmgr.h,SceSblACMgrForDriver_stub}
  */
 
-
 #ifndef _PSP2_KERNEL_SBLACMGR_H_
 #define _PSP2_KERNEL_SBLACMGR_H_
 
@@ -13,9 +12,88 @@
 extern "C" {
 #endif
 
+/**
+ * @brief Get process type state
+ *
+ * @param[in]  pid - The target process id
+ *
+ * @return If root mode process, 1. else 0.
+ */
+int ksceSblACMgrIsSystem(SceUID pid);
+
+#define ksceSblACMgrIsRootProgram(pid) ksceSblACMgrIsSystem(pid)
+
+/**
+ * @brief Get process type state
+ *
+ * @param[in]  pid - The target process id
+ *
+ * @return If system mode process, 1. else 0.
+ */
 int ksceSblACMgrIsShell(SceUID pid);
+
+#define ksceSblACMgrIsSystemProgram(pid) ksceSblACMgrIsShell(pid)
+
+/**
+ * @brief Get process type state
+ *
+ * @param[in]  pid - The target process id
+ *
+ * @return If game mode process, 1. else 0.
+ */
 int ksceSblACMgrIsGameProgram(SceUID pid);
+
+/**
+ * @brief Get process type state
+ *
+ * @param[in]  pid - The target process id
+ *
+ * @return If non game mode process, 1. else 0.
+ */
 int ksceSblACMgrIsNonGameProgram(SceUID pid);
+
+/**
+ * @brief Get process type state
+ *
+ * @param[in]  pid - The target process id
+ *
+ * @return If pspemu process, 1. else 0.
+ */
+int ksceSblACMgrIsPspEmu(SceUID pid);
+
+/**
+ * @brief Get process type state
+ *
+ * @param[in]  pid - The target process id
+ *
+ * @return If SceShell process(authid:0x2800000000000001), 1. else 0.
+ */
+int ksceSblACMgrIsSceShell(SceUID pid);
+
+/**
+ * @brief Get process type state
+ *
+ * @param[in]  pid - The target process id
+ *
+ * @return If fake self process, 1. else 0.
+ */
+int ksceSblACMgrIsFself(SceUID pid);
+
+/**
+ * @brief Get process authority id
+ *
+ * @param[in]  pid    - The target process id
+ * @param[out] authid - The authid output pointer
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int ksceSblACMgrGetProcessProgramAuthId(SceUID pid, SceUInt64 *authid);
+  
+/**
+ * @brief Get development mode state
+ *
+ * @return If development mode, 1. else 0.
+ */
 int ksceSblACMgrIsDevelopmentMode(void);
 
 #ifdef __cplusplus
@@ -23,4 +101,3 @@ int ksceSblACMgrIsDevelopmentMode(void);
 #endif
 
 #endif /* _PSP2_KERNEL_SBLACMGR_H_ */
-
