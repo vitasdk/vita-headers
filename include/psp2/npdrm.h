8072b3c9cb22076f4e83b30a80ecf1ac8818a398
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-11-27 09:22:55 +0900

    Add SceNpDrmLicense struct

--- include/psp2/npdrm.h
+++ include/psp2/npdrm.h
@@ -3,7 +3,6 @@
  * \usage{psp2/npdrm.h,SceNpDrm_stub}
  */
 
-
 #ifndef _PSP2_NPDRM_H_
 #define _PSP2_NPDRM_H_
 
@@ -13,6 +12,30 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct SceNpDrmLicense { // size is 0x200
+  SceInt16 version;       // -1, 1
+  SceInt16 version_flags; // 0, 1
+  SceInt16 license_type;  // 1
+  SceInt16 license_flags; // 0x400:non-check ecdsa
+  SceUInt64 account_id;
+  char content_id[0x30];
+  char key_table[0x10];
+  char key1[0x10];
+  SceInt64 start_time;
+  SceInt64 expiration_time;
+  char ecdsa_signature[0x28];
+  SceInt64 flags;
+  char key2[0x10];
+  char unk_0xB0[0x10];
+  char open_psid[0x10];
+  char unk_0xD0[0x10];
+  char cmd56_handshake_part[0x14];
+  int debug_upgradable;
+  int unk_0xF8;
+  int sku_flag;
+  char rsa_signature[0x100];
+} SceNpDrmLicense;
 
 /**
  * Get rif name
