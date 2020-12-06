ba16a8a72d78020a6cf5222b8c29d09e8103718a
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-12-06 13:22:52 +0900

    Create gpu_es4.h

---
+++ include/psp2kern/gpu_es4.h
@@ -0,0 +1,21 @@
+/**
+ * \kernelgroup{SceGpuEs4}
+ * \usage{psp2kern/gpu_es4.h,SceGpuEs4ForDriver}
+ */
+
+#ifndef _PSP2_KERNEL_GPU_ES4_
+#define _PSP2_KERNEL_GPU_ES4_
+
+#include <psp2kern/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int PVRSRVGetMiscInfoKM(void *dst, SceSize size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP2_KERNEL_GPU_ES4_ */
