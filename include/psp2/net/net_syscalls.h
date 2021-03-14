a26973f4aef65f7e76aaee35b30d2fd80ff3ceda
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2021-03-15 01:30:16 +0900

    Add sceNetSyscallGetIfList function (#568)
    * Add sceNetSyscallGetIfList function
    
    * Add types header

--- include/psp2/net/net_syscalls.h
+++ include/psp2/net/net_syscalls.h
@@ -8,10 +8,21 @@
 #define _PSP2_NET_NET_SYSCALLS_H_
 
 #include <psp2/net/net.h>
+#include <psp2/types.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/**
+ * @brief Get net if list
+ *
+ * @param[out] dst  - The output buffer list. size is (0x140 * size)
+ * @param[in]  size - The output size number.
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int sceNetSyscallGetIfList(void *dst, SceUInt32 size);
 
 int sceNetSyscallConnect(int s, const SceNetSockaddr *name, unsigned int namelen);
 
