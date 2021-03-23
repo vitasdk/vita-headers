468f6068f0d7bb29bff1e40ddf152d4efecd754f
Travis CI <builds@travis-ci.com>
2021-03-23 06:48:46 +0000

    Generated documents from TravisCI for build 1647

--- group__SceThreadMgrKernel_structSceKernelFaultingProcessInfo.js
+++ group__SceThreadMgrKernel_structSceKernelFaultingProcessInfo.js
@@ -1,5 +1,5 @@
 var group__SceThreadMgrKernel_structSceKernelFaultingProcessInfo =
 [
     [ "pid", "group__SceThreadMgrKernel.html#ga89e64d6ac24543f31f7645081269ed42", null ],
-    [ "unk", "group__SceThreadMgrKernel.html#ga0038b20a355c1b222212a071644c025d", null ]
+    [ "faultingThreadId", "group__SceThreadMgrKernel.html#ga52792d3d6f0a3b0344ee1dbe786365c4", null ]
 ];
