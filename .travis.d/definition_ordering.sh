bd6d9578b8ba4127315789e68e611e07569622df
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2021-05-16 16:11:32 +0900

    Update definition_ordering.sh

--- .travis.d/definition_ordering.sh
+++ .travis.d/definition_ordering.sh
@@ -3,7 +3,13 @@ set -e
 DB_FILE_PATH='./db.yml'
 LIBSEP="        \(functions\|variables\):" # name of the yaml block we want to check
 IFS=@
-# iterate over each block and `sort --check` they content
-for lib in $(grep "$LIBSEP\|          " "$DB_FILE_PATH" | sed "s/$LIBSEP/$IFS/"); do
-  printf "$lib" | grep -o "          [^:]*"| LC_ALL=C sort -c
+dir='./db/'
+for file in $dir; do
+  echo $file
+
+  # iterate over each block and `sort --check` they content
+  for lib in $(grep "$LIBSEP\|          " "$file" | sed "s/$LIBSEP/$IFS/"); do
+    printf "$lib" | grep -o "          [^:]*"| LC_ALL=C sort -c
+  done
 done
+
