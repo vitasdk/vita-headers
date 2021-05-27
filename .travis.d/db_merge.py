f1d981157abd0801a71df8f69c488b1a6a7b2cd3
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2021-05-28 08:29:23 +0900

    Add dbs sort code

--- .travis.d/db_merge.py
+++ .travis.d/db_merge.py
@@ -8,11 +8,18 @@ if __name__ == '__main__':
     db += "firmware: 3.60\n"
     db += "modules:\n"
 
+    file_list = []
+
     for i in range (0, len(files)):
+        file_list.append(files[i])
+
+    file_list.sort()
+
+    for i in range (0, len(file_list)):
 
         cache_char = ""
 
-        f = open(os.path.join(os.getcwd(), "db", files[i]), 'r')
+        f = open(os.path.join(os.getcwd(), "db", file_list[i]), 'r')
         for line in f:
             if line[:2] == "  ":
                 db += line
