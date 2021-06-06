90ada1655202e7af0610b70a50d2b2a7db9320ef
Sunguk Lee <d3m3vilurr@gmail.com>
2021-06-07 03:31:08 +0900

    build: Early exit at getting any building errors

--- build.py
+++ build.py
@@ -12,18 +12,21 @@ def execute(cmd, force_print=False):
         return r.close()
 
 def vita_libs_gen(yml, out):
-    execute('vita-libs-gen {} {}'.format(yml, out))
+    if execute('vita-libs-gen {} {}'.format(yml, out)):
+        raise SystemExit(10)
 
 def make(target):
     curr = os.getcwd()
     os.chdir(target)
-    execute('make -j')
+    if execute('make -j'):
+        raise SystemExit(11)
     os.chdir(curr)
 
 def make_install(target):
     curr = os.getcwd()
     os.chdir(target)
-    execute('make install')
+    if execute('make install'):
+        raise SystemExit(12)
     os.chdir(curr)
 
 def definition_check():
