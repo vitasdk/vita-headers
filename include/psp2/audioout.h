1190e801ec002d16085f085d331ba19f7a54f8ca
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2021-07-21 13:35:39 +0900

    Update documents for audioout

--- include/psp2/audioout.h
+++ include/psp2/audioout.h
@@ -77,11 +77,11 @@ typedef enum SceAudioOutAlcMode {
 /**
  * Initialize audio port
  *
- * @param[in] type - One of ::SceAudioOutPortType
+ * @param[in] portType - One of ::SceAudioOutPortType
  * @param[in] len - Number of samples, between ::SCE_AUDIO_MIN_LEN and ::SCE_AUDIO_MAX_LEN (multiple of 64)
  * @param[in] freq - Sampling frequency (in Hz), one of the followings :
  * 8000, 11025, 12000, 16000, 22050, 24000, 32000, 44100, 48000
- * @param[in] mode - One of ::SceAudioOutMode
+ * @param[in] param - One of ::SceAudioOutMode
  *
  * @return port number, < 0 on error.
  * @note - The volume is initially set to its max value (::SCE_AUDIO_OUT_MAX_VOL)
@@ -101,7 +101,7 @@ int sceAudioOutReleasePort(int port);
  * Output audio (blocking function)
  *
  * @param[in] port - Port number returned by ::sceAudioOutOpenPort
- * @param[in] *buf : Pointer to audio data buffer
+ * @param[in] ptr - Pointer to audio data buffer
  *
  * @return 0 on success, < 0 on error.
  * @note - if NULL is specified for *buf, the function will not return until the last
@@ -113,8 +113,8 @@ int sceAudioOutOutput(int port, const void *ptr);
  * Set volume of specified output audio port
  *
  * @param[in] port - Port number returned by ::sceAudioOutOpenPort
- * @param[in] ch - Channel numbers as flags (see ::SceAudioOutChannelFlag)
- * @param[in] *vol - Array to int specifying volume for each channel (Left channel first for stereo)
+ * @param[in] flag - Channel numbers as flags (see ::SceAudioOutChannelFlag)
+ * @param[in] vol - Array to int specifying volume for each channel (Left channel first for stereo)
  *
  * @return 0 on success, < 0 on error.
  */
@@ -126,7 +126,7 @@ int sceAudioOutSetVolume(int port, int flag, int *vol);
  * @param[in] port - Port number returned by ::sceAudioOutOpenPort
  * @param[in] len - see ::sceAudioOutOpenPort()
  * @param[in] freq - see ::sceAudioOutOpenPort()
- * @param[in] mode - see ::sceAudioOutOpenPort()
+ * @param[in] param - see ::sceAudioOutOpenPort()
  *
  * @return 0 on success, < 0 on error.
  * @note - If (-1) is specified for any argument (excepted for port), current configuration is used instead.
@@ -137,7 +137,7 @@ int sceAudioOutSetConfig(int port, int len, int freq, int param);
  * Get a parameter value of specified output port
  *
  * @param[in] port - Port number returned by ::sceAudioOutOpenPort
- * @param[in] type - One of ::SceAudioOutConfigType
+ * @param[in] configType - One of ::SceAudioOutConfigType
  *
  * @return 0 on success, < 0 on error.
  */
@@ -166,7 +166,7 @@ int sceAudioOutGetRestSample(int port);
  * Get status of port type
  * Return different value on whether the port type is used for sound generation or not.
  *
- * @param[in] type - One of ::SceAudioOutPortType
+ * @param[in] portType - One of ::SceAudioOutPortType
  *
  * @return (1) if port is in use, (0) otherwise. < 0 on error.
  */
