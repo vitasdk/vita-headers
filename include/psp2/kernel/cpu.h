311f1feee54d0be934c84007228217858216660f
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2021-07-21 11:51:41 +0900

    Create cpu.h

---
+++ include/psp2/kernel/cpu.h
@@ -0,0 +1,20 @@
+/**
+ * \usergroup{SceCpu}
+ * \usage{psp2/kernel/cpu.h,SceSysmem_stub}
+ */
+
+
+#ifndef _PSP2_KERNEL_CPU_H_
+#define _PSP2_KERNEL_CPU_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int sceKernelGetCpuId(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP2_KERNEL_CPU_H_ */
