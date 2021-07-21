98c7c9121c885009d7509cf5008b11221df805ac
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2021-07-21 12:18:26 +0900

    Update avcodec.h

--- include/psp2/avcodec.h
+++ include/psp2/avcodec.h
@@ -20,6 +20,19 @@ SceInt32 _sceVideoencQueryMemSize(SceUInt32 codecType, const SceVideodecQueryIni
 SceInt32 _sceVideoencQueryMemSizeInternal(SceUInt32 codecType, const SceVideodecQueryInitInfoInternal *pInitDecInfo, const SceVideoencQueryInitInfoInternal *pInitEncInfo, SceVideoencMemInfo *pMemInfo);
 SceInt32 _sceVideoencTermLibrary(SceUInt32 codecType);
 
+SceInt32 _sceM4vdecCreateDecoder(SceUInt32 codecType, SceM4vdecCtrl *pCtrl, const SceM4vdecQueryDecoderInfo *pDecoderInfo);
+SceInt32 _sceM4vdecCreateDecoderInternal(SceUInt32 codecType, SceM4vdecCtrl *pCtrl, const SceM4vdecQueryDecoderInfo *pDecoderInfo);
+SceInt32 _sceM4vdecCsc(SceM4vdecPicture *pDst, const SceM4vdecPicture *pSrc);
+SceInt32 _sceM4vdecDecode(const SceM4vdecCtrl *pCtrl, const SceM4vdecAu *pAu, SceM4vdecArrayPicture *pArrayPicture);
+SceInt32 _sceM4vdecDecodeAvailableSize(SceM4vdecCtrl *pCtrl);
+SceInt32 _sceM4vdecDecodeFlush(const SceM4vdecCtrl *pCtrl);
+SceInt32 _sceM4vdecDecodeStop(const SceM4vdecCtrl *pCtrl, SceM4vdecArrayPicture *pArrayPicture);
+SceInt32 _sceM4vdecDecodeStopWithWorkPicture(const SceM4vdecCtrl *pCtrl, SceM4vdecArrayPicture *pArrayPicture, SceM4vdecArrayPicture *pWorkArrayPicture);
+SceInt32 _sceM4vdecDecodeWithWorkPicture(const SceM4vdecCtrl *pCtrl, const SceM4vdecAu *pAu, SceM4vdecArrayPicture *pArrayPicture, SceM4vdecArrayPicture *pWorkArrayPicture);
+SceInt32 _sceM4vdecDeleteDecoder(SceM4vdecCtrl *pCtrl);
+SceInt32 _sceM4vdecQueryDecoderMemSize(SceUInt32 codecType, const SceM4vdecQueryDecoderInfo *pDecoderInfo, SceM4vdecDecoderInfo *pMemInfo);
+SceInt32 _sceM4vdecQueryDecoderMemSizeInternal(SceUInt32 codecType, const SceM4vdecQueryDecoderInfo *pDecoderInfo, SceM4vdecDecoderInfo *pMemInfo);
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
