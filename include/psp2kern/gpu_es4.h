d12218b9e937cb7c346bf9f84744ee746b535fe6
bythos14 <45720552+bythos14@users.noreply.github.com>
2021-09-04 21:12:57 -0400

    Add prototypes for SceGpu MMU functions

--- include/psp2kern/gpu_es4.h
+++ include/psp2kern/gpu_es4.h
@@ -16,6 +16,9 @@ int PVRSRVGetMiscInfoKM(void *info);
 
 int ksceGpuGetRegisterDump(void *dst, SceSize size);
 
+int ksceGpuMmuMapMemory(void *mmuContext, uint32_t vaddr, void *base, uint32_t size, uint32_t flags);
+int ksceGpuMmuUnmapMemory(void *mmuContext, uint32_t vaddr, uint32_t size);
+
 #ifdef __cplusplus
 }
 #endif
