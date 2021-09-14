834fc6e4bfa5e7be4d28e6bb61e312db6fe42a18
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2021-09-15 05:07:27 +0900

    Update sysmem
    split header and update function name

---
+++ include/psp2kern/kernel/sysmem/mmu.h
@@ -0,0 +1,53 @@
+/**
+ * \kernelgroup{SceSysmem}
+ * \usage{psp2kern/kernel/sysmem/mmu.h,SceSysmemForDriver_stub}
+ */
+
+#ifndef _PSP2_KERNEL_SYSMEM_MMU_H_
+#define _PSP2_KERNEL_SYSMEM_MMU_H_
+
+#include <psp2kern/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Get the physical address of a given virtual address
+ *
+ * @param[in]  va - The virtual address
+ * @param[out] pa - The physical address
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int ksceKernelVAtoPA(const void *va, uintptr_t *pa);
+
+/**
+ * Get the physical address list of a given virtual address range
+ *
+ * @param[in]  va_range  - The virtual address range
+ * @param[out] pa_vector - The vector of physical addresses
+ *
+ * @return 0 on success, < 0 on error.
+ */
+int ksceKernelVARangeToPAVector(const SceKernelVARange *va_range, SceKernelPAVector *pa_vector);
+
+typedef struct SceKernelProcessContext {
+	SceUInt32 TTBR1;
+	SceUInt32 DACR;
+	SceUInt32 CONTEXTIDR;
+} SceKernelProcessContext;
+
+int ksceKernelGetPidContext(SceUID pid, SceKernelProcessContext **ctx);
+
+#define ksceKernelGetPaddr(va, pa) ksceKernelVAtoPA(va, pa)
+#define ksceKernelGetPaddrList(input, list) ksceKernelVARangeToPAVector((const SceKernelVARange *)input, list)
+
+// Not exist on 3.60
+int ksceKernelSwitchVmaForPid(SceUID pid);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ include/psp2kern/kernel/sysmem/uid_puid.h
@@ -0,0 +1,53 @@
+/**
+ * \kernelgroup{SceSysmem}
+ * \usage{psp2kern/kernel/sysmem/uid_puid.h,SceSysmemForDriver_stub}
+ */
+
+#ifndef _PSP2_KERNEL_SYSMEM_UID_PUID_H_
+#define _PSP2_KERNEL_SYSMEM_UID_PUID_H_
+
+#include <psp2kern/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+ * Open Process UID by Global UID (Create PUID)
+ *
+ * param[in] pid  - The target process id.
+ * param[in] guid - The target GUID.
+ *
+ * return PUID on success, < 0 on error
+ */
+SceUID kscePUIDOpenByGUID(SceUID pid, SceUID guid);
+
+/*
+ * Close Process UID (Remove PUID)
+ *
+ * param[in] pid  - The target process id.
+ * param[in] puid - The target PUID.
+ *
+ * return PUID on success, < 0 on error
+ */
+int kscePUIDClose(SceUID pid, SceUID puid);
+
+/*
+ * Get Global UID by Process UID
+ *
+ * param[in] pid  - The target process id.
+ * param[in] puid - The target PUID.
+ *
+ * return GUID on success, < 0 on error
+ */
+SceUID kscePUIDtoGUID(SceUID pid, SceUID puid);
+
+#define ksceKernelKernelUidForUserUid(pid, puid) kscePUIDtoGUID(pid, puid)
+#define ksceKernelCreateUserUid(pid, guid) kscePUIDOpenByGUID(pid, guid)
+#define ksceKernelDeleteUserUid(pid, puid) kscePUIDClose(pid, puid)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
