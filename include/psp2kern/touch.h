d146bcdf6a2fcf1f15969b7eb327d64e50e49ad2
Princess-of-Sleeping <princess.of.sleeping.0x209@gmail.com>
2021-10-31 14:50:17 +0900

    Add ksceTouchSetEnableFlag function

---
+++ include/psp2kern/touch.h
@@ -0,0 +1,30 @@
+/**
+ * \kernelgroup{SceTouch}
+ * \usage{psp2kern/touch.h,SceTouchForDriver_stub}
+ */
+
+
+#ifndef _PSP2_KERNEL_TOUCH_H_
+#define _PSP2_KERNEL_TOUCH_H_
+
+#include <psp2kern/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Set touch enable flag
+ *
+ * @param[in] port   - The port number.
+ * @param[in] enable - The enable flag.
+ *
+ * @return 0 on success. < 0 on error.
+ */
+int ksceTouchSetEnableFlag(SceUInt32 port, SceBool enable);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP2_KERNEL_TOUCH_H_ */
