2b660ca19a7680aa1c562813e2812b9c8a012e4a
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2021-12-19 11:46:56 +0900

    Add ScePmMgr

---
+++ include/psp2kern/kernel/pm_mgr.h
@@ -0,0 +1,25 @@
+/**
+ * \kernelgroup{ScePmMgr}
+ * \usage{psp2kern/kernel/pm_mgr.h,ScePmMgrForDriver_stub}
+ */
+
+#ifndef _PSP2_KERNEL_PM_MGR_H_
+#define _PSP2_KERNEL_PM_MGR_H_
+
+#include <psp2kern/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef char SceProductMode;
+
+int kscePmMgrGetProductMode(SceProductMode *result);
+
+int kscePmMgrIsExternalBootMode(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP2_KERNEL_PM_MGR_H_ */
