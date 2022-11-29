d750cb3dce665e2693803fd567d95c5384fbf150
Princess-of-Sleeping <princess.of.sleeping.0x209@gmail.com>
2022-11-29 13:11:29 +0000

    Deploying to gh-pages from @ vitasdk/vita-headers@59840ab2f44f3f4d779ab14adda67c15e5bead19 🚀

--- group__SceCpuKernel.js
+++ group__SceCpuKernel.js
@@ -27,7 +27,7 @@ var group__SceCpuKernel =
     [ "ksceKernelRWSpinlockLowTryReadLock", "group__SceCpuKernel.html#ga2a8e345d257fb1643ccbb301820a6550", null ],
     [ "ksceKernelRWSpinlockLowReadUnlock", "group__SceCpuKernel.html#ga2e6aa771d998e8a9f40e85b892e1e691", null ],
     [ "ksceKernelRWSpinlockLowReadLockCpuSuspendIntr", "group__SceCpuKernel.html#ga4a9eb223ebfe1828ea649733c4da0f9d", null ],
-    [ "ksceKernelRWSpinlockLowReadTryLockCpuSuspendIntr", "group__SceCpuKernel.html#gae5bdcf6fa2da565f070ea9ca1982d4c4", null ],
+    [ "ksceKernelRWSpinlockLowTryReadLockCpuSuspendIntr", "group__SceCpuKernel.html#ga54368a6a84fb854135ab24d4cd9d7b0a", null ],
     [ "ksceKernelRWSpinlockLowReadUnlockCpuResumeIntr", "group__SceCpuKernel.html#ga5e37ece27ef1ca32822a9e7dfc9e9a6f", null ],
     [ "ksceKernelRWSpinlockLowWriteLock", "group__SceCpuKernel.html#gaf577b4cab05e17d9f97be7db7107e1ae", null ],
     [ "ksceKernelRWSpinlockLowTryWriteLock", "group__SceCpuKernel.html#ga8dc432901323dd9d6f2286cb823a97e9", null ],
