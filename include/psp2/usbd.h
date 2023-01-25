351723f00c09188c6140c5cbe87344a83804a296
Sunguk Lee <d3m3vilurr@gmail.com>
2023-01-25 14:15:13 +0900

    psp2: Fix indentations and remove some debug comments of USB headers

--- include/psp2/usbd.h
+++ include/psp2/usbd.h
@@ -14,49 +14,49 @@ extern "C" {
 #endif
 
 typedef enum SceUsbdErrorCode {
-    SCE_USBD_ERROR_NOT_INITIALIZED     = 0x80240001,
-    SCE_USBD_ERROR_ALREADY_INITIALIZED = 0x80240002,
-    SCE_USBD_ERROR_INVALID_PARAM       = 0x80240003,
+	SCE_USBD_ERROR_NOT_INITIALIZED     = 0x80240001,
+	SCE_USBD_ERROR_ALREADY_INITIALIZED = 0x80240002,
+	SCE_USBD_ERROR_INVALID_PARAM       = 0x80240003,
 
-    SCE_USBD_ERROR_PIPE_NOT_FOUND      = 0x80240004,
+	SCE_USBD_ERROR_PIPE_NOT_FOUND      = 0x80240004,
 
-    SCE_USBD_ERROR_NO_MEMORY           = 0x80240005,
-    SCE_USBD_ERROR_DEVICE_NOT_FOUND    = 0x80240006,
+	SCE_USBD_ERROR_NO_MEMORY           = 0x80240005,
+	SCE_USBD_ERROR_DEVICE_NOT_FOUND    = 0x80240006,
 
-    SCE_USBD_ERROR_80240007            = 0x80240007, //
-    SCE_USBD_ERROR_80240009            = 0x80240009, //
-    SCE_USBD_ERROR_8024000A            = 0x8024000A, //
+	SCE_USBD_ERROR_80240007            = 0x80240007,
+	SCE_USBD_ERROR_80240009            = 0x80240009,
+	SCE_USBD_ERROR_8024000A            = 0x8024000A,
 
-    SCE_USBD_ERROR_FATAL               = 0x802400FF
+	SCE_USBD_ERROR_FATAL               = 0x802400FF
 } SceUsbdErrorCode;
 
 typedef struct SceUsbdDeviceInfo {
-    unsigned int port;
-    unsigned int device_num;
-    unsigned int unk3; // handled? 0, 1, 2
+	unsigned int port;
+	unsigned int device_num;
+	unsigned int unk3; // handled? 0, 1, 2
 } SceUsbdDeviceInfo;
 
 typedef struct SceUsbdTransferData {
-    unsigned int pipe; // 0x00
-    const void *data; // 0x04
-    unsigned int data_size;  // 0x08
-    void *transferred; //  // 0x0C // ptr to 8 bytes?
-    unsigned int timeout;  // 0x10 // size of above ptr? only 8 allowed?
+	unsigned int pipe; // 0x00
+	const void *data; // 0x04
+	unsigned int data_size;  // 0x08
+	void *transferred; //  // 0x0C // ptr to 8 bytes?
+	unsigned int timeout;  // 0x10 // size of above ptr? only 8 allowed?
 } SceUsbdTransferData; /* size = 0x14 */
 
 typedef struct SceUsbdReceiveEvent {
-    unsigned int unk0; // 0
-    unsigned int unk1; // next ptr?
-    unsigned int unk2; // != 8, set to 1, 2, 4? // transfer flags? type?
-    unsigned int unk3; // ??
-    unsigned int unk4; // ??
-    unsigned int unk5; // ??
-    unsigned int transfer_id; // ??
+	unsigned int unk0; // 0
+	unsigned int unk1; // next ptr?
+	unsigned int unk2; // != 8, set to 1, 2, 4? // transfer flags? type?
+	unsigned int unk3;
+	unsigned int unk4;
+	unsigned int unk5;
+	unsigned int transfer_id;
 } SceUsbdReceiveEvent; /* size = 0x1C */
 
 typedef struct SceUsbdDeviceAddress {
-    unsigned int unk0;
-    unsigned short unk1;
+	unsigned int unk0;
+	unsigned short unk1;
 } SceUsbdDeviceAddress; /* size = 0x6 */
 
 #define USB_DESCRIPTOR_DEVICE                   0x01    // bDescriptorType for a Device Descriptor.
@@ -141,46 +141,46 @@ int sceUsbdGetDescriptor(SceUID uid, SceUID device_id, unsigned char *descriptor
 #define SCE_USBD_CLASS_VENDOR_SPECIFIC               0xff
 
 typedef enum SceUsbdDescriptorType {
-    SCE_USBD_DESCRIPTOR_DEVICE            = 0x01,    // bDescriptorType for a Device Descriptor.
-    SCE_USBD_DESCRIPTOR_CONFIGURATION     = 0x02,    // bDescriptorType for a Configuration Descriptor.
-    SCE_USBD_DESCRIPTOR_STRING            = 0x03,    // bDescriptorType for a String Descriptor.
-    SCE_USBD_DESCRIPTOR_INTERFACE         = 0x04,    // bDescriptorType for an Interface Descriptor.
-    SCE_USBD_DESCRIPTOR_ENDPOINT          = 0x05,    // bDescriptorType for an Endpoint Descriptor.
+	SCE_USBD_DESCRIPTOR_DEVICE            = 0x01,    // bDescriptorType for a Device Descriptor.
+	SCE_USBD_DESCRIPTOR_CONFIGURATION     = 0x02,    // bDescriptorType for a Configuration Descriptor.
+	SCE_USBD_DESCRIPTOR_STRING            = 0x03,    // bDescriptorType for a String Descriptor.
+	SCE_USBD_DESCRIPTOR_INTERFACE         = 0x04,    // bDescriptorType for an Interface Descriptor.
+	SCE_USBD_DESCRIPTOR_ENDPOINT          = 0x05,    // bDescriptorType for an Endpoint Descriptor.
 
-    SCE_USBD_DESCRIPTOR_DEVICE_QUALIFIER  = 0x06,    // bDescriptorType for a Device Qualifier.
-    SCE_USBD_DESCRIPTOR_OTHER_SPEED       = 0x07,    // bDescriptorType for a Other Speed Configuration.
-    SCE_USBD_DESCRIPTOR_INTERFACE_POWER   = 0x08,    // bDescriptorType for Interface Power.
-    SCE_USBD_DESCRIPTOR_OTG               = 0x09,    // bDescriptorType for an OTG Descriptor.
-    SCE_USBD_DESCRIPTOR_HID               = 0x21,    // bDescriptorType for an HID descriptor.
-    SCE_USBD_DESCRIPTOR_REPORT            = 0x22     // bDescriptorType for an HID report descriptor.
+	SCE_USBD_DESCRIPTOR_DEVICE_QUALIFIER  = 0x06,    // bDescriptorType for a Device Qualifier.
+	SCE_USBD_DESCRIPTOR_OTHER_SPEED       = 0x07,    // bDescriptorType for a Other Speed Configuration.
+	SCE_USBD_DESCRIPTOR_INTERFACE_POWER   = 0x08,    // bDescriptorType for Interface Power.
+	SCE_USBD_DESCRIPTOR_OTG               = 0x09,    // bDescriptorType for an OTG Descriptor.
+	SCE_USBD_DESCRIPTOR_HID               = 0x21,    // bDescriptorType for an HID descriptor.
+	SCE_USBD_DESCRIPTOR_REPORT            = 0x22     // bDescriptorType for an HID report descriptor.
 } SceUsbdDescriptorType;
 
 typedef struct SceUsbdDeviceDescriptor {
-    uint8_t bLength;
-    uint8_t bDescriptorType;
-    uint16_t bcdUSB;
-    uint8_t bDeviceClass;
-    uint8_t bDeviceSubclass;
-    uint8_t bDeviceProtocol;
-    uint8_t bMaxPacketSize0;
-    uint16_t idVendor;
-    uint16_t idProduct;
-    uint16_t bcdDevice;
-    uint8_t iManufacturer;
-    uint8_t iProduct;
-    uint8_t iSerialNumber;
-    uint8_t bNumConfigurations;
+	uint8_t bLength;
+	uint8_t bDescriptorType;
+	uint16_t bcdUSB;
+	uint8_t bDeviceClass;
+	uint8_t bDeviceSubclass;
+	uint8_t bDeviceProtocol;
+	uint8_t bMaxPacketSize0;
+	uint16_t idVendor;
+	uint16_t idProduct;
+	uint16_t bcdDevice;
+	uint8_t iManufacturer;
+	uint8_t iProduct;
+	uint8_t iSerialNumber;
+	uint8_t bNumConfigurations;
 } SceUsbdDeviceDescriptor;
 
 typedef struct SceUsbdConfigurationDescriptor {
-    uint8_t bLength;
-    uint8_t bDescriptorType;
-    uint16_t wTotalLength;
-    uint8_t bNumInterfaces;
-    uint8_t bConfigurationValue;
-    uint8_t iConfiguration;
-    uint8_t bmAttributes;
-    uint8_t MaxPower;
+	uint8_t bLength;
+	uint8_t bDescriptorType;
+	uint16_t wTotalLength;
+	uint8_t bNumInterfaces;
+	uint8_t bConfigurationValue;
+	uint8_t iConfiguration;
+	uint8_t bmAttributes;
+	uint8_t MaxPower;
 } SceUsbdConfigurationDescriptor;
 
 #define SCE_USBD_CONFIGURATION_RESERVED_ZERO         0x1f
@@ -189,24 +189,24 @@ typedef struct SceUsbdConfigurationDescriptor {
 #define SCE_USBD_CONFIGURATION_RESERVED_ONE          0x80
 
 typedef struct SceUsbdInterfaceDescriptor {
-    uint8_t bLength;
-    uint8_t bDescriptorType;
-    uint8_t bInterfaceNumber;
-    uint8_t bAlternateSetting;
-    uint8_t bNumEndpoints;
-    uint8_t bInterfaceClass;
-    uint8_t bInterfaceSubclass;
-    uint8_t bInterfaceProtocol;
-    uint8_t iInterface;
+	uint8_t bLength;
+	uint8_t bDescriptorType;
+	uint8_t bInterfaceNumber;
+	uint8_t bAlternateSetting;
+	uint8_t bNumEndpoints;
+	uint8_t bInterfaceClass;
+	uint8_t bInterfaceSubclass;
+	uint8_t bInterfaceProtocol;
+	uint8_t iInterface;
 } SceUsbdInterfaceDescriptor;
 
 typedef struct SceUsbdEndpointDescriptor {
-    uint8_t bLength;
-    uint8_t bDescriptorType;
-    uint8_t bEndpointAddress;
-    uint8_t bmAttributes;
-    uint16_t wMaxPacketSize;
-    uint8_t bInterval;
+	uint8_t bLength;
+	uint8_t bDescriptorType;
+	uint8_t bEndpointAddress;
+	uint8_t bmAttributes;
+	uint16_t wMaxPacketSize;
+	uint8_t bInterval;
 } SceUsbdEndpointDescriptor;
 
 /* bmAttributes */
@@ -227,25 +227,25 @@ typedef struct SceUsbdEndpointDescriptor {
 #define SCE_USBD_ENDPOINT_DIRECTION_IN               0x80
 
 typedef struct SceUsbdStringDescriptor {
-    uint8_t bLength;
-    uint8_t bDescriptorType;
-    uint8_t bString[0];
+	uint8_t bLength;
+	uint8_t bDescriptorType;
+	uint8_t bString[0];
 } SceUsbdStringDescriptor;
 
 typedef struct SceUsbdHidSubDescriptorInfo {
-    uint8_t bDescriptorType;
-    uint8_t wDescriptorLength0;
-    uint8_t wDescriptorLength1;
+	uint8_t bDescriptorType;
+	uint8_t wDescriptorLength0;
+	uint8_t wDescriptorLength1;
 } SceUsbdHidSubDescriptorInfo;
 
 typedef struct SceUsbdHidDescriptor {
-    uint8_t bLength;
-    uint8_t bDescriptorType;
-    uint8_t bcdHID0;
-    uint8_t bcdHID1;
-    uint8_t bCountryCode;
-    uint8_t bNumDescriptors;  /* SubDescriptor count */
-    SceUsbdHidSubDescriptorInfo SubDescriptorInfo[0];
+	uint8_t bLength;
+	uint8_t bDescriptorType;
+	uint8_t bcdHID0;
+	uint8_t bcdHID1;
+	uint8_t bCountryCode;
+	uint8_t bNumDescriptors;  /* SubDescriptor count */
+	SceUsbdHidSubDescriptorInfo SubDescriptorInfo[0];
 } SceUsbdHidDescriptor;
 
 
@@ -329,17 +329,17 @@ int sceUsbdAttach(SceUID uid, SceUID driver_id, SceUInt bus, SceUInt device);
 int sceUsbdGetDeviceAddress(SceUID uid, SceUID device_id, SceUsbdDeviceAddress *addr);
 
 typedef struct SceUsbdTransferStatus {
-    uint32_t unk0; // < 0x40 - transfer id (result of sceUsbdTransferData)
-    uint32_t unk1; // unused
-    uint32_t unk2; // ret 4. ptr? // status? 
-    uint32_t unk3; // ret 4. ptr? // transferred?
+	uint32_t unk0; // < 0x40 - transfer id (result of sceUsbdTransferData)
+	uint32_t unk1; // unused
+	uint32_t unk2; // ret 4. ptr? // status?
+	uint32_t unk3; // ret 4. ptr? // transferred?
 } SceUsbdTransferStatus;
 
 typedef struct SceUsbdIsochTransferStatus {
-    uint32_t unk0; // < 0x40 - size? pipe? transfer id?
-    uint32_t unk1; // unused
-    uintptr_t* unk2; // ret up to 0x28 buff. 10 * 4 bytes. or 8*5 bytes
-    uint32_t unk3; // ret 4. ptr?
+	uint32_t unk0; // < 0x40 - size? pipe? transfer id?
+	uint32_t unk1; // unused
+	uintptr_t* unk2; // ret up to 0x28 buff. 10 * 4 bytes. or 8*5 bytes
+	uint32_t unk3; // ret 4. ptr?
 } SceUsbdIsochTransferStatus;
 
 /**
@@ -367,12 +367,12 @@ int sceUsbdGetTransferStatus(SceUID transfer_id, SceUsbdTransferStatus* status);
 int sceUsbdGetIsochTransferStatus(SceUID transfer_id, SceUsbdIsochTransferStatus* status);
 
 typedef struct SceUsbdDevicePipe {
-    unsigned int device_id;
-    unsigned int unk1;
-    unsigned int unk2;
-    unsigned int unk3;
-    unsigned int unk4;
-    unsigned int unk5;
+	unsigned int device_id;
+	unsigned int unk1;
+	unsigned int unk2;
+	unsigned int unk3;
+	unsigned int unk4;
+	unsigned int unk5;
 } SceUsbdDevicePipe; /* size = 0x18 */
 
 /**
@@ -420,18 +420,18 @@ int sceUsbdClosePipe(SceUID uid, SceUID pipe_id);
 SceUID sceUsbdTransferData(SceUID uid, SceUsbdTransferData *data);
 
 typedef struct SceUsbdIsochTransfer {
-    unsigned int unk0; // array of num_packets? ptrs to 4 
-    unsigned int unk1; // num packets?
-    unsigned int unk2;
-    unsigned int unk3;
+	unsigned int unk0; // array of num_packets? ptrs to 4
+	unsigned int unk1; // num packets?
+	unsigned int unk2;
+	unsigned int unk3;
 
-    unsigned int unk4;
-    unsigned int unk5;
-    unsigned int unk6;
-    unsigned int unk7;
+	unsigned int unk4;
+	unsigned int unk5;
+	unsigned int unk6;
+	unsigned int unk7;
 
-    unsigned int unk8;
-    unsigned int unk9;
+	unsigned int unk8;
+	unsigned int unk9;
 } SceUsbdIsochTransfer; /* size = 0x28 */
 
 /**
@@ -491,11 +491,11 @@ SceUID sceUsbdRegisterCompositeLdd(SceUID uid, char* name);
 int sceUsbdUnregisterLdd(SceUID uid, char* name);
 
 typedef struct SceUsbdAttachCompositeParam {
-    uint32_t driver_id;
-    uint32_t bus;
-    uint32_t device;
-    uint32_t unk3; // num devices?
-    uint32_t unk4;
+	uint32_t driver_id;
+	uint32_t bus;
+	uint32_t device;
+	uint32_t unk3; // num devices?
+	uint32_t unk4;
 } SceUsbdAttachCompositeParam; /* size = 0x14 */
 
 /**
