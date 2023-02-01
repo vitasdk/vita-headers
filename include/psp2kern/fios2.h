96ef69266d9e6246991cac4fe2715522e5e31f58
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2023-02-01 16:40:00 +0900

    Added fios2kernel02.h
    And move shared defines.

--- include/psp2kern/fios2.h
+++ include/psp2kern/fios2.h
@@ -7,43 +7,12 @@
 #define _PSP2KERN_FIOS2_H_
 
 #include <psp2kern/types.h>
+#include <psp2common/fios2.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define SCE_FIOS2_OVERLAY_PATH_SIZE       (292)
-#define SCE_FIOS2_OVERLAY_PATH_MAX_LENGTH (SCE_FIOS2_OVERLAY_PATH_SIZE - 1)
-
-typedef int32_t SceFiosOverlayID;
-
-typedef enum SceFiosOverlayType {
-  // src replaces dst. All accesses to dst are redirected to src.
-  SCE_FIOS_OVERLAY_TYPE_OPAQUE      = 0,
-
-  // src merges with dst. Reads check src first, then dst. Writes go to dst.
-  SCE_FIOS_OVERLAY_TYPE_TRANSLUCENT = 1,
-
-  // src merges with dst. Reads check both src and dst, and use whichever has the most recent modification time.
-  // If both src and dst have the same modification time, dst is used.
-  // If no file exists at src or dst, dst is used; if no file exists at dst, but a file exists at src, src is used. Writes go to dst.
-  SCE_FIOS_OVERLAY_TYPE_NEWER       = 2,
-
-  // src merges with dst. Reads check src first, then dst. Writes go to src.
-  SCE_FIOS_OVERLAY_TYPE_WRITABLE    = 3
-} SceFiosOverlayType;
-
-typedef struct SceFiosOverlay {
-  uint8_t type; // see SceFiosOverlayType
-  uint8_t order;
-  uint16_t dst_len;
-  uint16_t src_len;
-  uint16_t unk2;
-  SceUID pid;
-  SceFiosOverlayID id;
-  char dst[SCE_FIOS2_OVERLAY_PATH_SIZE];
-  char src[SCE_FIOS2_OVERLAY_PATH_SIZE]; // src path replaces dst path based on type policy
-} SceFiosOverlay;
 
 /**
  * Overlay process file system overlay
@@ -88,6 +57,7 @@ int ksceFiosKernelOverlayRemoveForProcess(SceUID pid, SceFiosOverlayID id);
  * @return     Error code or zero on success
  */
 int ksceFiosKernelOverlayResolveSync(SceUID pid, int resolveFlag, const char *inPath, char *outPath, SceSize maxPath);
+
 
 #ifdef __cplusplus
 }
