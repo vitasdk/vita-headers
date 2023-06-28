78ede6980b966bce164975259479e3ab83ccab10
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2023-06-28 22:46:56 +0900

    Move processmgr shared define to psp2common

--- check_size/psp2all.c
+++ check_size/psp2all.c
@@ -6,6 +6,37 @@
 #include <psp2/ctrl.h>
 #include <psp2kern/ctrl.h>
 
+// #include <psp2/net/net.h>
+// #include <psp2kern/net/net.h>
+
+// #include <psp2/kernel/threadmgr.h>
+// #include <psp2kern/kernel/threadmgr.h>
+
+// #include <psp2/kernel/modulemgr.h>
+// #include <psp2kern/kernel/modulemgr.h>
+
+#include <psp2/kernel/processmgr.h>
+#include <psp2kern/kernel/processmgr.h>
+#include <psp2kern/kernel/suspend.h>
+
+// #include <psp2/rtc.h>
+// #include <psp2kern/kernel/rtc.h>
+
+// #include <psp2/jpegenc.h>
+// #include <psp2kern/avcodec/jpegenc.h>
+
+// #include <psp2/display.h>
+// #include <psp2kern/display.h>
+
+// #include <psp2/power.h>
+// #include <psp2kern/power.h>
+
+// #include <psp2/udcd.h>
+// #include <psp2kern/udcd.h>
+
+// #include <psp2/usbd.h>
+// #include <psp2kern/usbd.h>
+
 __attribute__ ((used)) const unsigned int version = PSP2_SDK_VERSION;
 
 void _start() __attribute__ ((weak, alias("module_start")));
