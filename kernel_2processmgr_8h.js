565f8ff2f0836ebf1727daef698f728b88954e23
d3m3vilurr <d3m3vilurr@gmail.com>
2023-06-29 14:33:47 +0000

    Deploying to gh-pages from @ vitasdk/vita-headers@78ede6980b966bce164975259479e3ab83ccab10 🚀

--- kernel_2processmgr_8h.js
+++ kernel_2processmgr_8h.js
@@ -1,29 +1,7 @@
 var kernel_2processmgr_8h =
 [
-    [ "SceKernelClock", "group__SceProcessmgrUser.html#gaa7c492e33c3601c8e7897247a5b4830a", null ],
-    [ "SceKernelTime", "group__SceProcessmgrUser.html#ga5fd5556a01c9d9cc7b987a4172c1d28a", null ],
-    [ "SceKernelProcessPrioritySystem", "group__SceProcessmgrUser.html#ga83160683af360c4aa067a6c45698eeea", [
-      [ "SCE_KERNEL_PROCESS_PRIORITY_SYSTEM_HIGH", "group__SceProcessmgrUser.html#gga83160683af360c4aa067a6c45698eeeaab07f03401c26dedfd28f5f8268ae71c5", null ],
-      [ "SCE_KERNEL_PROCESS_PRIORITY_SYSTEM_DEFAULT", "group__SceProcessmgrUser.html#gga83160683af360c4aa067a6c45698eeeaa8b2aeb151c6fa6f3f42d2e941f543ce7", null ],
-      [ "SCE_KERNEL_PROCESS_PRIORITY_SYSTEM_LOW", "group__SceProcessmgrUser.html#gga83160683af360c4aa067a6c45698eeeaadf8592aec4baadd219790186d7414c78", null ]
-    ] ],
-    [ "SceKernelProcessPriorityUser", "group__SceProcessmgrUser.html#ga30e79a41f091de99f1c307269f8d4f8a", [
-      [ "SCE_KERNEL_PROCESS_PRIORITY_USER_HIGH", "group__SceProcessmgrUser.html#gga30e79a41f091de99f1c307269f8d4f8aa8021b6335684c6c7264304d77b24bb70", null ],
-      [ "SCE_KERNEL_PROCESS_PRIORITY_USER_DEFAULT", "group__SceProcessmgrUser.html#gga30e79a41f091de99f1c307269f8d4f8aaaafba4c71a432612eb68d3bddda50e56", null ],
-      [ "SCE_KERNEL_PROCESS_PRIORITY_USER_LOW", "group__SceProcessmgrUser.html#gga30e79a41f091de99f1c307269f8d4f8aaf0052e483c47e105ee26335f6ec69ff8", null ]
-    ] ],
-    [ "SceKernelPowerTickType", "group__SceProcessmgrUser.html#ga1b448620c7525b730a2b94164e34d77f", [
-      [ "SCE_KERNEL_POWER_TICK_DEFAULT", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fae71baa308d1cf3469b372f41a74de0e9", null ],
-      [ "SCE_KERNEL_POWER_TICK_DISABLE_AUTO_SUSPEND", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fa3e8b9af3aaeb7cd8fb6ce66a8063d872", null ],
-      [ "SCE_KERNEL_POWER_TICK_DISABLE_OLED_OFF", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fa61d6ff07f428ecf1f42ecb4644002d39", null ],
-      [ "SCE_KERNEL_POWER_TICK_DISABLE_OLED_DIMMING", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fa55cba5df6d5a94e9b0188c47696cb650", null ],
-      [ "SCE_KERNEL_POWER_TICK_DEFAULT", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fae71baa308d1cf3469b372f41a74de0e9", null ],
-      [ "SCE_KERNEL_POWER_TICK_DISABLE_AUTO_SUSPEND", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fa3e8b9af3aaeb7cd8fb6ce66a8063d872", null ],
-      [ "SCE_KERNEL_POWER_TICK_DISABLE_OLED_OFF", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fa61d6ff07f428ecf1f42ecb4644002d39", null ],
-      [ "SCE_KERNEL_POWER_TICK_DISABLE_OLED_DIMMING", "group__SceSuspendKernel.html#gga1b448620c7525b730a2b94164e34d77fa55cba5df6d5a94e9b0188c47696cb650", null ]
-    ] ],
-    [ "VITASDK_BUILD_ASSERT_EQ", "group__SceProcessmgrUser.html#ga6ba2f413501bd9b371900ea1e90d12a5", null ],
-    [ "VITASDK_BUILD_ASSERT_EQ", "group__SceProcessmgrUser.html#ga56a8ee6e33ddb92fd7c595a6f39545ed", null ],
+    [ "VITASDK_BUILD_ASSERT_EQ", "group__SceProcessmgrUser.html#ga9096b15aff065259faae93c7ce23f577", null ],
+    [ "VITASDK_BUILD_ASSERT_EQ", "group__SceProcessmgrUser.html#ga34870f1e1be9758fbad54254ab5aee26", null ],
     [ "sceKernelExitProcess", "group__SceProcessmgrUser.html#gaf64b2cd0fc96095e3f77dee2dc9a68c3", null ],
     [ "sceKernelPowerTick", "group__SceProcessmgrUser.html#ga2255f042fe267ddd8b38eee5fa578e33", null ],
     [ "sceKernelPowerLock", "group__SceProcessmgrUser.html#ga2cd2b3124993e2b420003f7d9d6dc5a8", null ],
@@ -39,7 +17,5 @@ var kernel_2processmgr_8h =
     [ "sceKernelGetProcessParam", "group__SceProcessmgrUser.html#ga3eb82309ef0b6a3e4e2dd233635ffd35", null ],
     [ "sceKernelLibcClock", "group__SceProcessmgrUser.html#gaa06b7d896c6611f1250b60711ed065fd", null ],
     [ "sceKernelLibcTime", "group__SceProcessmgrUser.html#ga2ce94ab54870f46ed95bbb2917be4477", null ],
-    [ "VITASDK_BUILD_ASSERT_EQ", "group__SceProcessmgrUser.html#ga9096b15aff065259faae93c7ce23f577", null ],
-    [ "VITASDK_BUILD_ASSERT_EQ", "group__SceProcessmgrUser.html#ga34870f1e1be9758fbad54254ab5aee26", null ],
     [ "sceKernelLibcGettimeofday", "group__SceProcessmgrUser.html#gac268657dfbfea56250ac6438f24ad149", null ]
 ];
---
+++ ommon_2kernel_2processmgr_8h.js
@@ -0,0 +1,25 @@
+var ommon_2kernel_2processmgr_8h =
+[
+    [ "SceKernelProcessType", "group__SceProcessmgrKernel.html#ga337851539a6ec21c154d8fbdadc63eaf", null ],
+    [ "SceKernelClock", "group__SceProcessmgrKernel.html#gaa7c492e33c3601c8e7897247a5b4830a", null ],
+    [ "SceKernelTime", "group__SceProcessmgrKernel.html#ga5fd5556a01c9d9cc7b987a4172c1d28a", null ],
+    [ "SceKernelProcessPrioritySystem", "group__SceProcessmgrKernel.html#gadb0815fce3d3cf9df1db406fff0cfb75", [
+      [ "SCE_KERNEL_PROCESS_PRIORITY_SYSTEM_HIGH", "group__SceProcessmgrKernel.html#ggadb0815fce3d3cf9df1db406fff0cfb75ab07f03401c26dedfd28f5f8268ae71c5", null ],
+      [ "SCE_KERNEL_PROCESS_PRIORITY_SYSTEM_DEFAULT", "group__SceProcessmgrKernel.html#ggadb0815fce3d3cf9df1db406fff0cfb75a8b2aeb151c6fa6f3f42d2e941f543ce7", null ],
+      [ "SCE_KERNEL_PROCESS_PRIORITY_SYSTEM_LOW", "group__SceProcessmgrKernel.html#ggadb0815fce3d3cf9df1db406fff0cfb75adf8592aec4baadd219790186d7414c78", null ]
+    ] ],
+    [ "SceKernelProcessPriorityUser", "group__SceProcessmgrKernel.html#ga67ad7728ad471d5389eeab3c59dc9b9b", [
+      [ "SCE_KERNEL_PROCESS_PRIORITY_USER_HIGH", "group__SceProcessmgrKernel.html#gga67ad7728ad471d5389eeab3c59dc9b9ba8021b6335684c6c7264304d77b24bb70", null ],
+      [ "SCE_KERNEL_PROCESS_PRIORITY_USER_DEFAULT", "group__SceProcessmgrKernel.html#gga67ad7728ad471d5389eeab3c59dc9b9baaafba4c71a432612eb68d3bddda50e56", null ],
+      [ "SCE_KERNEL_PROCESS_PRIORITY_USER_LOW", "group__SceProcessmgrKernel.html#gga67ad7728ad471d5389eeab3c59dc9b9baf0052e483c47e105ee26335f6ec69ff8", null ]
+    ] ],
+    [ "SceKernelPowerTickType", "group__SceProcessmgrKernel.html#ga8fb37ca0dc2c31c9a74c873fd2686560", [
+      [ "SCE_KERNEL_POWER_TICK_DEFAULT", "group__SceProcessmgrKernel.html#gga8fb37ca0dc2c31c9a74c873fd2686560ae71baa308d1cf3469b372f41a74de0e9", null ],
+      [ "SCE_KERNEL_POWER_TICK_DISABLE_AUTO_SUSPEND", "group__SceProcessmgrKernel.html#gga8fb37ca0dc2c31c9a74c873fd2686560a3e8b9af3aaeb7cd8fb6ce66a8063d872", null ],
+      [ "SCE_KERNEL_POWER_TICK_DISABLE_OLED_OFF", "group__SceProcessmgrKernel.html#gga8fb37ca0dc2c31c9a74c873fd2686560a61d6ff07f428ecf1f42ecb4644002d39", null ],
+      [ "SCE_KERNEL_POWER_TICK_DISABLE_OLED_DIMMING", "group__SceProcessmgrKernel.html#gga8fb37ca0dc2c31c9a74c873fd2686560a55cba5df6d5a94e9b0188c47696cb650", null ]
+    ] ],
+    [ "VITASDK_BUILD_ASSERT_EQ", "group__SceProcessmgrKernel.html#gae49572e6f71ee3efca6804195100a742", null ],
+    [ "VITASDK_BUILD_ASSERT_EQ", "group__SceProcessmgrKernel.html#gae8866e954e641aa111f4a94b6d7b69b8", null ],
+    [ "VITASDK_BUILD_ASSERT_EQ", "group__SceProcessmgrKernel.html#gac396217ca06fc5f539a8e1ed3b470689", null ]
+];
