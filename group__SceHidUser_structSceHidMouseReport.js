d3411358896995207d46d4053a4771f6ce4ae040
Princess-of-Sleeping <princess.of.sleeping.0x209@gmail.com>
2023-07-18 08:18:15 +0000

    Deploying to gh-pages from @ vitasdk/vita-headers@c0a7a04e04338529bc8c7e2cda6b33934e1fc2d6 🚀

--- group__SceHidUser_structSceHidMouseReport.js
+++ group__SceHidUser_structSceHidMouseReport.js
@@ -4,5 +4,7 @@ var group__SceHidUser_structSceHidMouseReport =
     [ "reserved", "group__SceHidUser.html#ga25f11dc21a634387abfb62ea3f389bcb", null ],
     [ "rel_x", "group__SceHidUser.html#gac2932af16fd9ee7297bee234f0552fc1", null ],
     [ "rel_y", "group__SceHidUser.html#ga71717cfd6f60ee2087f7953baf200c77", null ],
-    [ "unk", "group__SceHidUser.html#gaf30daf00a9fb1b67337c62f4ee71ee8a", null ]
+    [ "wheel", "group__SceHidUser.html#gaae8f669b363e40d2fd3d6f75935ca853", null ],
+    [ "tilt", "group__SceHidUser.html#ga122fe1cfcff07d5d03f9a9990181a155", null ],
+    [ "timestamp", "group__SceHidUser.html#gafb173425829bac46eefb0c81f066d8b2", null ]
 ];
