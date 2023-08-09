27d86e71b27bfe0663c09222e5c60ac90ab57c0c
Princess-of-Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2023-08-10 03:18:03 +0900

    Move udcd shared define to psp2common

--- include/psp2/udcd.h
+++ include/psp2/udcd.h
@@ -9,47 +9,11 @@
 
 #include <vitasdk/build_utils.h>
 #include <psp2/types.h>
+#include <psp2common/udcd.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/** USB Status */
-typedef enum SceUdcdStatus {
-	SCE_UDCD_STATUS_CONNECTION_NEW          = 0x0001,
-	SCE_UDCD_STATUS_CONNECTION_ESTABLISHED  = 0x0002,
-	SCE_UDCD_STATUS_CONNECTION_SUSPENDED    = 0x0004,
-	SCE_UDCD_STATUS_CABLE_DISCONNECTED      = 0x0010,
-	SCE_UDCD_STATUS_CABLE_CONNECTED         = 0x0020,
-	SCE_UDCD_STATUS_DEACTIVATED             = 0x0100,
-	SCE_UDCD_STATUS_ACTIVATED               = 0x0200,
-	SCE_UDCD_STATUS_IS_CHARGING             = 0x0400,
-	SCE_UDCD_STATUS_USE_USB_CHARGING        = 0x0800,
-	SCE_UDCD_STATUS_UNKNOWN_1000            = 0x1000,
-	SCE_UDCD_STATUS_UNKNOWN_2000            = 0x2000
-} SceUdcdStatus;
-
-/** USB Driver status
- */
-typedef enum SceUdcdStatusDriver {
-	SCE_UDCD_STATUS_DRIVER_STARTED         = 0x01,
-	SCE_UDCD_STATUS_DRIVER_REGISTERED      = 0x02
-} SceUdcdStatusDriver;
-
-typedef struct {
-	uint8_t info[64];
-} SceUdcdDeviceInfo;
-VITASDK_BUILD_ASSERT_EQ(0x40, SceUdcdDeviceInfo);
-
-typedef struct SceUdcdWaitParam {
-	int unk_00;
-	int status;
-	int unk_08;
-	int unk_0C;
-	int unk_10;
-	const char *driverName;
-} SceUdcdWaitParam;
-VITASDK_BUILD_ASSERT_EQ(0x18, SceUdcdWaitParam);
 
 typedef struct SceUdcdDeviceState {
 	int unk_00;
