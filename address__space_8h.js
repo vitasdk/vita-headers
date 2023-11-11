78501798b35bef81a3d13810c0b1e5ed8c91fdea
Princess-of-Sleeping <princess.of.sleeping.0x209@gmail.com>
2023-11-11 02:39:55 +0000

    Deploying to gh-pages from @ vitasdk/vita-headers@26169751e513f6bb08b49787bbfa9fe9c37eb618 🚀

--- address__space_8h.js
+++ address__space_8h.js
@@ -5,6 +5,7 @@ var address__space_8h =
     [ "ksceKernelGetPaddr", "group__SceSysmemKernel.html#ga08919a4ad0eade0704d3849a07760b4b", null ],
     [ "ksceKernelGetPaddrList", "group__SceSysmemKernel.html#gab2cd2942139ad596a6a4e5cb8123763f", null ],
     [ "ksceKernelVAtoPA", "group__SceSysmemKernel.html#ga1d08cf0a1dc1804d7ae09763c8aab190", null ],
+    [ "ksceKernelVARangeToPARange", "group__SceSysmemKernel.html#ga1325e76b82c1388bbb5c83a387435660", null ],
     [ "ksceKernelVARangeToPAVector", "group__SceSysmemKernel.html#ga2c25db542655e83ab9e43c1971beaf2a", null ],
     [ "VITASDK_BUILD_ASSERT_EQ", "group__SceSysmemKernel.html#ga51da91157080bf920fabfc83e88ec210", null ],
     [ "ksceKernelProcessGetContext", "group__SceSysmemKernel.html#gabfea04df41dcebba993db73a0901af54", null ],
--- uid__class_8h.js
+++ uid__class_8h.js
@@ -1,12 +1,17 @@
 var uid__class_8h =
 [
+    [ "ksceKernelCreateClass", "group__SceSysmemKernel.html#ga1ec80f40d8ddfa9b69aea08bb441ee40", null ],
+    [ "ksceKernelGetUidClass", "group__SceSysmemKernel.html#gab9b345d2fa110f176e7e28eec391ac3d", null ],
+    [ "ksceKernelGetUidDLinkClass", "group__SceSysmemKernel.html#ga373010ce3daf6c34e0af821234848d56", null ],
+    [ "ksceKernelGetUidHeapClass", "group__SceSysmemKernel.html#ga29a9b26f7989b786cee6261c0e8436ab", null ],
+    [ "ksceKernelGetUidMemBlockClass", "group__SceSysmemKernel.html#ga09d6a977dca811d685bfdc373af4f618", null ],
     [ "SceClassCallback", "group__SceSysmemKernel.html#gac04de86fb0a37624c069a3311358c9f5", null ],
     [ "VITASDK_BUILD_ASSERT_EQ", "group__SceSysmemKernel.html#gaf47659144240f9dbf6d6f3363bf7136d", null ],
     [ "VITASDK_BUILD_ASSERT_EQ", "group__SceSysmemKernel.html#gac8abc561016cc95e7e55fb3e9dbbe17f", null ],
-    [ "ksceKernelGetUidClass", "group__SceSysmemKernel.html#ga31ac66ef8604284f9fd7c2dd717e6360", null ],
-    [ "ksceKernelGetUidDLinkClass", "group__SceSysmemKernel.html#ga46fe504bf586cd94155c2eef35bc8517", null ],
-    [ "ksceKernelGetUidHeapClass", "group__SceSysmemKernel.html#ga9127eaa3b3458b14e529ee170d527fa8", null ],
-    [ "ksceKernelGetUidMemBlockClass", "group__SceSysmemKernel.html#ga2b673592a631dcca87499f2e1cf450e4", null ],
-    [ "ksceKernelCreateClass", "group__SceSysmemKernel.html#ga85ff61a056f3376c377bc640539348ea", null ],
+    [ "ksceKernelGetUIDClass", "group__SceSysmemKernel.html#gafaa79ec7806555c9719aad8aece49995", null ],
+    [ "ksceKernelGetUIDDLinkClass", "group__SceSysmemKernel.html#gaf0c517de1393018d1f95e21e3c1ff19c", null ],
+    [ "ksceKernelGetUIDHeapClass", "group__SceSysmemKernel.html#gab0988716bf7283a56699f40bd1aae495", null ],
+    [ "ksceKernelGetUIDMemBlockClass", "group__SceSysmemKernel.html#ga0d23a298c45dd1c69815db32f32d0ead", null ],
+    [ "ksceUIDClassInitClass", "group__SceSysmemKernel.html#gac4c0ba4297138e503fc971a1f3e89ff4", null ],
     [ "ksceKernelFindClassByName", "group__SceSysmemKernel.html#ga0b6b5ddadfa9a82287945b5bc04cd0de", null ]
 ];
