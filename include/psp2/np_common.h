de15ec6ba0c7321470268bce98da36ef21ed9148
olebeck <31539311+olebeck@users.noreply.github.com>
2026-03-02 13:04:54 +0100

    move SceNpCommunicationId to a common header

---
+++ include/psp2/np_common.h
@@ -0,0 +1,29 @@
+/**
+ * \usergroup{SceNp}
+ * \usage{psp2/np_common.h}
+ */
+
+
+#ifndef _PSP2_NP_COMMON_H_
+#define _PSP2_NP_COMMON_H_
+
+#include <vitasdk/build_utils.h>
+#include <psp2/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct SceNpCommunicationId {
+	char data[9];
+	char term;
+	SceUChar8 num;
+	char dummy;
+} SceNpCommunicationId;
+VITASDK_BUILD_ASSERT_EQ(0xC, SceNpCommunicationId);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _PSP2_NP_COMMON_H_ */
